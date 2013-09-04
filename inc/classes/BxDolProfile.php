be4f7b4783622610d3839d0a75b42cabca9a4fee
Alex Trofimov <alex.trofimov@boonex.com>
2013-09-04 19:17:16 +1000

    #9

--- inc/classes/BxDolProfile.php
+++ inc/classes/BxDolProfile.php
@@ -171,8 +171,11 @@ class BxDolProfile extends BxDol {
 
     /**
      * Delete profile.
+     * @param $ID - optional profile id to delete
+     * @param $bForceDelete - force deletetion is case of account profile deletion
+     * @return false on error, or true on success
      */
-    function delete($ID = false) {
+    function delete($ID = false, $bForceDelete = false) {
 
         $ID = (int)$ID;
         if (!$ID)
@@ -181,6 +184,19 @@ class BxDolProfile extends BxDol {
         $aProfileInfo = $this->_oQuery->getInfoById($ID);
         if (!$aProfileInfo)
             return false;
+
+        // delete system profiles (accounts) is not allowed, instead - delete whole account
+        if (!$bForceDelete && 'system' == $aProfileInfo['type'])
+            return false;
+    
+        // switch profile context if deleted profile is active profile context
+        bx_import('BxDolAccount');
+        $oAccount = BxDolAccount::getInstance ($aProfileInfo['account_id']);
+        $aAccountInfo = $oAccount->getInfo();
+        if (!$bForceDelete && $ID == $aAccountInfo['profile_id']) {
+            $oProfileAccount = BxDolProfile::getInstanceAccountProfile($aProfileInfo['account_id']);
+            $oAccount->updateProfileContext($oProfileAccount->id());
+        }
 
         // create system event before deletion
         $isStopDeletion = false;
