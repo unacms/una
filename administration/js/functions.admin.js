dc95f9d61ce170a414d04080a5388432d109001e
Alex Trofimov <alex.trofimov@boonex.com>
2013-09-22 15:40:36 +1000

    #20 - old code was removed, anyway this code is based on old Dolphin 7.0.6, if these files will be needed, it can be taken from Dolphin 7.1
    - administration folder
    
    - Twig classes
    - Text classes
    
    - articles module
    - news module
    - events module
    - groups module
    - simple messenger module
    - spy module
    - membership module
    - payment module
    
    - basic template module
    - sundance template module
    
    - Akismet plugin
    - moovrelocator plugin
    - myadmin/zip plugin

--- administration/js/functions.admin.js
+++
@@ -1,7 +0,0 @@
-
-function adminMenuCollapse(oImage) {
-    if($(oImage).parents('.adm-menu-header').hasClass('adm-mmh-opened'))
-        $(oImage).removeClass('adm-mma-opened').parents('.adm-menu-header').removeClass('adm-mmh-opened').siblings('.adm-menu-items-wrapper').removeClass('adm-mmi-opened');
-    else
-        $(oImage).addClass('adm-mma-opened').parents('.adm-menu-header').addClass('adm-mmh-opened').siblings('.adm-menu-items-wrapper').addClass('adm-mmi-opened');
-}
--- modules/boonex/basic/install/data/templates/tmpl_basic/scripts/BxTemplName.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package     Dolphin Core
- * @copyright   Copyright (c) BoonEx Pty Limited - http://www.boonex.com/
- * @license     CC-BY - http://creativecommons.org/licenses/by/3.0/
- */
-defined('BX_DOL') or die('hack attempt');
-
-$sTemplName = 'Basic';
