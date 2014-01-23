066c7792cae84036f42e61c88b001b88d80e83c3
Alex Trofimov <alex.trofimov@boonex.com>
2014-01-23 22:06:18 +1100

    #37 - fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,11 @@
 <?php
 
+define('BX_SKIP_INSTALL_CHECK', 1);
+
 $aPathInfo = pathinfo(__FILE__);
 $sHeaderPath = $aPathInfo['dirname'] . '/../inc/header.inc.php';
 if (!file_exists($sHeaderPath))
     die("Script is not installed\n");
+
 require_once($sHeaderPath);
 
