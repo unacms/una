71dd616464d0d50f6866402b8611806f9c8c47f6
Alex Trofimov <alex.trofimov@boonex.com>
2014-02-05 22:11:13 +1100

    #41 - SMTP Mailer refactoring

--- modules/boonex/smtpmailer/classes/BxSMTPTemplate.php
+++ modules/boonex/smtpmailer/classes/BxSMTPTemplate.php
@@ -1,4 +1,4 @@
-<?php
+<?php defined('BX_DOL') or die('hack attempt');
 /**
  * Copyright (c) BoonEx Pty Limited - http://www.boonex.com/
  * CC-BY License - http://creativecommons.org/licenses/by/3.0/
