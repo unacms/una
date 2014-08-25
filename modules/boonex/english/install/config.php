86cb5ff783cf15bf2b31f8d3cfd6cdae617d92e9
AntonLV <vlnotna@gmail.com>
2014-08-25 15:54:39 +0600

    Modules' version number update.

--- modules/boonex/english/install/config.php
+++ modules/boonex/english/install/config.php
@@ -17,7 +17,7 @@ $aConfig = array(
     'name' => 'bx_en',
     'title' => 'English',
     'note' => 'Language file',
-    'version' => '8.0.1.DEV',
+    'version' => '8.0.2.DEV',
     'vendor' => 'Boonex',
 	'help_url' => 'http://feed.boonex.com/?section={module_name}',
 
