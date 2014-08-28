cd4cff9534d81e12ab0adc19a1d965638e1abbb8
AntonLV <vlnotna@gmail.com>
2014-08-28 16:49:33 +0600

    Modules' version number update.

--- modules/boonex/developer/install/config.php
+++ modules/boonex/developer/install/config.php
@@ -17,7 +17,7 @@ $aConfig = array(
     'name' => 'bx_developer',
     'title' => 'Developer',
     'note' => 'Developer tools...',
-    'version' => '8.0.1.DEV',
+    'version' => '8.0.2.DEV',
     'vendor' => 'BoonEx',
 	'help_url' => 'http://feed.boonex.com/?section={module_name}',
 
