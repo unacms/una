42258354a242b5ce6c717e3a36e5792b879a5c9a
Alex Trofimov <alex.trofimov@boonex.com>
2014-09-01 22:45:19 +0600

    #81 add boonex key&secret to install page

--- install/sql/addon.sql
+++ install/sql/addon.sql
@@ -10,6 +10,8 @@ INSERT INTO `sys_profiles` (`account_id`, `type`, `content_id`, `status`) VALUES
 UPDATE `sys_options` SET `VALUE` = '{admin_email}' WHERE `Name` = 'site_email';
 UPDATE `sys_options` SET `VALUE` = '{site_title}' WHERE `Name` = 'site_title';
 UPDATE `sys_options` SET `VALUE` = '{site_email}' WHERE `Name` = 'site_email_notify';
+UPDATE `sys_options` SET `VALUE` = '{oauth_key}' WHERE `Name` = 'sys_oauth_key';
+UPDATE `sys_options` SET `VALUE` = '{oauth_secret}' WHERE `Name` = 'sys_oauth_secret';
 UPDATE `sys_options` SET `VALUE` = '{time}' WHERE `Name` = 'sys_install_time';
 
 UPDATE `sys_modules` SET `version` = '{version}' WHERE `name` = 'system';
