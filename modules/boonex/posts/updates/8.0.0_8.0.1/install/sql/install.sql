8009a929a7138b81b0e62cc1af3f5f554b38820c
Alex Trofimov <alex.trofimov@boonex.com>
2014-10-07 17:48:22 +0600

    Modules' updates.

--- modules/boonex/posts/updates/8.0.0_8.0.1/install/sql/install.sql
+++ modules/boonex/posts/updates/8.0.0_8.0.1/install/sql/install.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `bx_posts_meta_locations` (
+CREATE TABLE IF NOT EXISTS `bx_posts_meta_locations` (
   `object_id` int(10) unsigned NOT NULL,
   `lat` double NOT NULL,
   `lng` double NOT NULL,
@@ -10,14 +10,17 @@ CREATE TABLE `bx_posts_meta_locations` (
   KEY `country_state_city` (`country`,`state`(8),`city`(8))
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-INSERT INTO `sys_form_inputs`(`object`, `module`, `name`, `value`, `values`, `checked`, `type`, `caption_system`, `caption`, `info`, `required`, `collapsed`, `html`, `attrs`, `attrs_tr`, `attrs_wrapper`, `checker_func`, `checker_params`, `checker_error`, `db_pass`, `db_params`, `editable`, `deletable`) VALUES 
+
+INSERT IGNORE INTO `sys_form_inputs`(`object`, `module`, `name`, `value`, `values`, `checked`, `type`, `caption_system`, `caption`, `info`, `required`, `collapsed`, `html`, `attrs`, `attrs_tr`, `attrs_wrapper`, `checker_func`, `checker_params`, `checker_error`, `db_pass`, `db_params`, `editable`, `deletable`) VALUES 
 ('bx_posts', 'bx_posts', 'location', '', '', 0, 'custom', '_sys_form_input_sys_location', '_sys_form_input_location', '', 0, 0, 0, '', '', '', '', '', '', '', '', 0, 0);
 
 UPDATE `sys_form_display_inputs` SET `order`='5' WHERE `display_name`='bx_posts_entry_add' AND `input_name`='allow_view_to' LIMIT 1;
-INSERT INTO `sys_form_display_inputs` (`display_name`, `input_name`, `visible_for_levels`, `active`, `order`) VALUES ('bx_posts_entry_add', 'location', 2147483647, 1, 6);
+INSERT IGNORE INTO `sys_form_display_inputs` (`display_name`, `input_name`, `visible_for_levels`, `active`, `order`) VALUES 
+('bx_posts_entry_add', 'location', 2147483647, 1, 6);
 
 UPDATE `sys_form_display_inputs` SET `order`='5' WHERE `display_name`='bx_posts_entry_edit' AND `input_name`='allow_view_to' LIMIT 1;
-INSERT INTO `sys_form_display_inputs` (`display_name`, `input_name`, `visible_for_levels`, `active`, `order`) VALUES ('bx_posts_entry_edit', 'location', 2147483647, 1, 6);
+INSERT IGNORE INTO `sys_form_display_inputs` (`display_name`, `input_name`, `visible_for_levels`, `active`, `order`) VALUES 
+('bx_posts_entry_edit', 'location', 2147483647, 1, 6);
 
 UPDATE `sys_objects_cmts` SET `BaseUrl`='page.php?i=view-post&id={object_id}' WHERE `Name`='bx_posts' LIMIT 1;
 
@@ -25,6 +28,8 @@ UPDATE `sys_pages_blocks` SET `designbox_id`='3' WHERE `object`='bx_posts_view_e
 UPDATE `sys_pages_blocks` SET `designbox_id`='3' WHERE `object`='bx_posts_view_entry' AND `title`='_bx_posts_page_block_title_entry_actions' LIMIT 1;
 UPDATE `sys_pages_blocks` SET `designbox_id`='3' WHERE `object`='bx_posts_author' AND `title`='_bx_posts_page_block_title_entries_actions' LIMIT 1;
 
-INSERT INTO `sys_objects_search` (`ObjectName`, `Title`, `ClassName`, `ClassPath`) VALUES ('bx_posts_cmts', '_bx_posts_cmts', 'BxPostsCmtsSearchResult', 'modules/boonex/posts/classes/BxPostsCmtsSearchResult.php');
+DELETE FROM `sys_objects_search` WHERE `ObjectName` = 'bx_posts_cmts';
+INSERT INTO `sys_objects_search` (`ObjectName`, `Title`, `ClassName`, `ClassPath`) VALUES 
+('bx_posts_cmts', '_bx_posts_cmts', 'BxPostsCmtsSearchResult', 'modules/boonex/posts/classes/BxPostsCmtsSearchResult.php');
 
 UPDATE `sys_objects_metatags` SET `table_locations`='bx_posts_meta_locations' WHERE `object`='bx_posts' LIMIT 1;
