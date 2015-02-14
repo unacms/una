af59f4a4a1eff76fe8c69cfb976dae254b824386
Alex Trofimov <alex.trofimov@boonex.com>
2015-02-14 19:21:27 +0600

    Upgrade Patch 8.0.0-A10 to 8.0.0-A11

--- upgrade/files/8.0.0.A10-8.0.0.A11/check.php
+++ upgrade/files/8.0.0.A10-8.0.0.A11/check.php
@@ -1,5 +1,10 @@
 <?php
 
+bx_import('BxDolLanguagesQuery');
+bx_import('BxDolEmailTemplatesQuery');
+bx_import('BxDolTemplate');
+bx_import('BxDolAccountQuery');
+
 $mixCheckResult = 'Update can not be applied';
 
 if ('8.0.0-A10' == $this->oDb->getOne("SELECT `version` FROM `sys_modules` WHERE `name` = 'system'"))
