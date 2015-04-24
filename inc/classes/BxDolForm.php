90ae61029dcf8fba4a93e29c04bd8513e5c8bced
Alex Trofimov <alex.trofimov@boonex.com>
2015-04-24 19:07:40 +0600

    #167 emails with long 1st level domain names aren't validating (such as .link)

--- inc/classes/BxDolForm.php
+++ inc/classes/BxDolForm.php
@@ -1342,7 +1342,7 @@ class BxDolFormCheckerHelper
     }
     static public function checkEmail($s)
     {
-        return self::checkPreg ($s, '/^[a-z0-9_\-]+(\.[_a-z0-9\-]+)*@([_a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel)$/i');
+        return self::checkPreg ($s, "/(([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?/");
     }
     static public function checkCaptcha($s)
     {
