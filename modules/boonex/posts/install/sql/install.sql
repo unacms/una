988662091ec85885fdda97fa717f83a13ff1fb83
AntonLV <vlnotna@gmail.com>
2015-10-23 10:08:25 +0300

    Ticket #32.- Votes system improvements.

--- modules/boonex/posts/install/sql/install.sql
+++ modules/boonex/posts/install/sql/install.sql
@@ -83,11 +83,13 @@ CREATE TABLE IF NOT EXISTS `bx_posts_votes` (
 ) ENGINE=MYISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `bx_posts_votes_track` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `object_id` int(11) NOT NULL default '0',
   `author_id` int(11) NOT NULL default '0',
   `author_nip` int(11) unsigned NOT NULL default '0',
   `value` tinyint(4) NOT NULL default '0',
   `date` int(11) NOT NULL default '0',
+  PRIMARY KEY (`id`),
   KEY `vote` (`object_id`, `author_nip`)
 ) ENGINE=MYISAM DEFAULT CHARSET=utf8;
 
