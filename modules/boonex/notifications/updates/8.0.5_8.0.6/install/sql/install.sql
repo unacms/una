2f27ed0214bd9167eb846ce2fb1e72e1cf37f162
AntonLV <vlnotna@gmail.com>
2015-11-23 13:19:24 +0300

    Modules' updates.

--- modules/boonex/notifications/updates/8.0.5_8.0.6/install/sql/install.sql
+++ modules/boonex/notifications/updates/8.0.5_8.0.6/install/sql/install.sql
@@ -1,10 +1,13 @@
 ALTER TABLE `bx_notifications_events` ADD `subobject_id` int(11) NOT NULL default '0' AFTER `object_privacy_view`;
 ALTER TABLE `bx_notifications_events` ADD `processed` tinyint(4) NOT NULL default '0' AFTER `date`;
 
+
 ALTER TABLE `bx_notifications_handlers` ADD `group` varchar(64) NOT NULL default '' AFTER `id`;
+
+
+UPDATE `bx_notifications_handlers` SET `group`='profile' WHERE `alert_unit`='profile' AND `alert_action`='delete' LIMIT 1;
+
 
 ALTER TABLE `bx_notifications_handlers` DROP INDEX `handler`;
 ALTER TABLE `bx_notifications_handlers` ADD UNIQUE `alert` (`alert_unit`, `alert_action`);
 ALTER TABLE `bx_notifications_handlers` ADD UNIQUE `handler` (`group`, `type`);
-
-UPDATE `bx_notifications_handlers` SET `group`='profile' WHERE `alert_unit`='profile' AND `alert_action`='delete' LIMIT 1;
