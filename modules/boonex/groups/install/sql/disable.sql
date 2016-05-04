120061703b51ae91c170bc8bd6f9bf224ace7f97
Alex Trofimov <alex.trofimov@boonex.com>
2016-05-04 10:27:18 +0600

    #281 Groups: removing friends related staff

--- modules/boonex/groups/install/sql/disable.sql
+++ modules/boonex/groups/install/sql/disable.sql
@@ -10,7 +10,7 @@ DELETE FROM `sys_options_types` WHERE `id` = @iTypeId;
 -- PAGES
 
 DELETE FROM `sys_objects_page` WHERE `module` = 'bx_groups';
-DELETE FROM `sys_pages_blocks` WHERE `module` = 'bx_groups' OR `object` IN('bx_groups_create_profile', 'bx_groups_delete_profile', 'bx_groups_edit_profile', 'bx_groups_edit_profile_cover', 'bx_groups_view_profile', 'bx_groups_profile_info', 'bx_groups_profile_friends', 'bx_groups_home', 'bx_groups_fans');
+DELETE FROM `sys_pages_blocks` WHERE `module` = 'bx_groups' OR `object` IN('bx_groups_create_profile', 'bx_groups_delete_profile', 'bx_groups_edit_profile', 'bx_groups_edit_profile_cover', 'bx_groups_view_profile', 'bx_groups_profile_info', 'bx_groups_home', 'bx_groups_fans');
 
 -- MENU
 
