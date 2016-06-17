d9be7882260a9ba93f79b8dbe3b297d71ccee03d
Alex Trofimov <alex.trofimov@boonex.com>
2016-06-17 13:58:37 +0600

    #255 Clean up: installation design fixes

--- install/templates/_page.php
+++ install/templates/_page.php
@@ -23,23 +23,25 @@
 </head>
 <body class="bx-def-font bx-def-color-bg-page">
 
-<div class="bx-install-page">
+<div class="bx-install-page bx-main">
 
-    <div id="bx-toolbar" class="bx-def-z-index-nav">
-        <div class="bx-def-centered bx-def-padding-sec-left bx-def-padding-sec-right bx-def-page-width">
-            <div id="bx-toolbar-content" class="bx-clearfix">
-                <div id="bx-menu-toolbar-1-container"></div>
-                <div id="bx-logo-container"><a class="bx-def-font-contrasted"><?=$sTitle; ?></a></div>
-                <div id="bx-menu-toolbar-2-container">
-                    <?php if (!empty($aToolbarItem)): ?>
-                    <ul class="bx-menu-toolbar bx-menu-object-sys_toolbar_member">
-                        <li class="bx-def-color-bg-hl-hover ">
-                            <a title="<?=$aToolbarItem['title']; ?>" href="<?=$aToolbarItem['link']; ?>" target="<?=$aToolbarItem['target']; ?>" onclick="" class="bx-def-margin-sec-left bx-def-font-contrasted">
-                                <i class="sys-icon <?=$aToolbarItem['icon']; ?> bx-def-margin-sec-leftright"></i>
-                            </a>
-                        </li>
-                    </ul>
-                    <?php endif; ?>
+    <div id="bx-toolbar" class="bx-header bx-def-image-bg-header bx-def-color-bg-header bx-def-border-header bx-shadow-header bx-def-z-index-nav bx-toolbar-anim bx-toolbar-reset headroom--top">
+        <div class="bx-def-centered bx-def-padding-sec-leftright bx-def-page-width bx-def-box-sizing">
+            <div class="bx-toolbar-content-wrapper bx-content-padding-header">
+                <div id="bx-toolbar-content" class="bx-clearfix">
+                    <div id="bx-menu-toolbar-1-container"></div>
+                    <div id="bx-logo-container"><a class="bx-def-font-contrasted"><?=$sTitle; ?></a></div>
+                    <div id="bx-menu-toolbar-2-container">
+                        <?php if (!empty($aToolbarItem)): ?>
+                        <ul class="bx-menu-toolbar bx-menu-object-sys_toolbar_member">
+                            <li class="bx-def-color-bg-hl-hover ">
+                                <a title="<?=$aToolbarItem['title']; ?>" href="<?=$aToolbarItem['link']; ?>" target="<?=$aToolbarItem['target']; ?>" onclick="" class="bx-def-margin-sec-left bx-def-font-contrasted">
+                                    <i class="sys-icon <?=$aToolbarItem['icon']; ?> bx-def-margin-sec-leftright"></i>
+                                </a>
+                            </li>
+                        </ul>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
         </div>
@@ -47,7 +49,7 @@
 
     <div id="bx-menu-main-bar-wrapper"></div>
 
-    <div class="bx-page-wrapper bx-def-centered bx-def-padding-sec-left bx-def-padding-sec-right bx-def-page-width">
+    <div class="bx-page-wrapper bx-menu-toolbar-padding bx-def-centered bx-def-padding-sec-left bx-def-padding-sec-right bx-def-page-width">
         <div id="bx-content-container" class="bx-def-padding-sec-left bx-def-padding-sec-right">
             <div id="bx-content-main" class="bx-def-margin-top">
                 <?=$sCode; ?>
