5b6e05af198121156ddff72e2f32ecefa0e4b44d
AntonLV <vlnotna@gmail.com>
2016-06-22 10:24:21 +0300

    Ticket #285 - ACL Module: Studio.

--- modules/boonex/acl/install/sql/disable.sql
+++ modules/boonex/acl/install/sql/disable.sql
@@ -13,11 +13,11 @@ DELETE FROM `sys_menu_items` WHERE `module` = @sName;
 
 
 -- SETTINGS
-SET @iTypeId = (SELECT `ID` FROM `sys_options_types` WHERE `name` = @sName LIMIT 1);
-SET @iCategId = (SELECT `ID` FROM `sys_options_categories` WHERE `type_id` = @iTypeId LIMIT 1);
-DELETE FROM `sys_options` WHERE `category_id` = @iCategId;
-DELETE FROM `sys_options_categories` WHERE `type_id` = @iTypeId;
-DELETE FROM `sys_options_types` WHERE `id` = @iTypeId;
+-- SET @iTypeId = (SELECT `ID` FROM `sys_options_types` WHERE `name` = @sName LIMIT 1);
+-- SET @iCategId = (SELECT `ID` FROM `sys_options_categories` WHERE `type_id` = @iTypeId LIMIT 1);
+-- DELETE FROM `sys_options` WHERE `category_id` = @iCategId;
+-- DELETE FROM `sys_options_categories` WHERE `type_id` = @iTypeId;
+-- DELETE FROM `sys_options_types` WHERE `id` = @iTypeId;
 
 
 -- ALERTS
