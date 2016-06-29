c59f381cb3ae6baae581899d015ecd1a1ce6397c
AntonLV <vlnotna@gmail.com>
2016-06-29 16:35:16 +0300

    Modules updates.

--- modules/boonex/notifications/updates/8.0.6_9.0.0/install/installer.php
+++ modules/boonex/notifications/updates/8.0.6_9.0.0/install/installer.php
@@ -6,9 +6,13 @@
 
 class BxNtfsUpdater extends BxDolStudioUpdater
 {
+	protected $_aModule;
+
     function __construct($aConfig)
 	{
         parent::__construct($aConfig);
+
+        $this->_aModule = $this->oDb->getModuleByUri($this->_aConfig['module_uri']);
     }
 
 	public function actionExecuteSql($sOperation)
@@ -25,5 +29,22 @@ class BxNtfsUpdater extends BxDolStudioUpdater
 		}
 
     	return parent::actionExecuteSql($sOperation);
+    }
+
+	protected function actionClearDbCache($sOperation)
+    {
+    	if($sOperation == 'install') {
+	    	$aModules = $this->oDb->getModulesBy(array('type' => 'all', 'active' => 1));
+		    foreach($aModules as $aModule) {
+		    	$aConfig = self::getModuleConfig($aModule);
+				if(empty($aConfig['relations']) || !is_array($aConfig['relations']) || !in_array($this->_aModule['name'], $aConfig['relations'])) 
+					continue;	
+
+				BxDolService::call($this->_aModule['name'], 'delete_handlers', array($aModule['uri']));
+				BxDolService::call($this->_aModule['name'], 'add_handlers', array($aModule['uri']));
+			}
+    	}
+
+    	return parent::actionClearDbCache($sOperation);
     }
 }
