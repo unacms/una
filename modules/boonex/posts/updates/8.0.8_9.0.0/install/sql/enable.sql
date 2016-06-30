f8cfc64c776208d0134066bc94870c55d123366f
AntonLV <vlnotna@gmail.com>
2016-06-30 15:09:19 +0300

    Modules updates: Posts.

--- modules/boonex/posts/updates/8.0.8_9.0.0/install/sql/enable.sql
+++ modules/boonex/posts/updates/8.0.8_9.0.0/install/sql/enable.sql
@@ -10,7 +10,7 @@ INSERT INTO `sys_pages_blocks`(`object`, `cell_id`, `module`, `title_system`, `t
 UPDATE `sys_pages_blocks` SET `content`='a:4:{s:6:\"module\";s:6:\"system\";s:6:\"method\";s:15:\"categories_list\";s:6:\"params\";a:2:{i:0;s:13:\"bx_posts_cats\";i:1;a:1:{s:10:\"show_empty\";b:1;}}s:5:\"class\";s:20:\"TemplServiceCategory\";}' WHERE `object`='bx_posts_home' AND `title`='_bx_posts_page_block_title_cats';
 
 SET @iPBCellProfile = 3;
-DELETE FROM `sys_pages_blocks` WHERE `object`='trigger_page_profile_view_entry' AND `title`='_bx_posts_page_block_title_my_entries';
+DELETE FROM `sys_pages_blocks` WHERE (`object`='trigger_page_profile_view_entry' OR `module`='bx_posts') AND `title`='_bx_posts_page_block_title_my_entries';
 INSERT INTO `sys_pages_blocks` (`object`, `cell_id`, `module`, `title`, `designbox_id`, `visible_for_levels`, `type`, `content`, `deletable`, `copyable`, `order`) VALUES
 ('trigger_page_profile_view_entry', @iPBCellProfile, 'bx_posts', '_bx_posts_page_block_title_my_entries', 11, 2147483647, 'service', 'a:3:{s:6:"module";s:8:"bx_posts";s:6:"method";s:13:"browse_author";s:6:"params";a:1:{i:0;s:12:"{profile_id}";}}', 0, 0, 0);
 
