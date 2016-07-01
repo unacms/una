7e1e17564386a22dc4687dfdb81581cc18b26ec6
AntonLV <vlnotna@gmail.com>
2016-07-01 13:00:56 +0300

    Ticket #323: Update Relations during Installation.

--- modules/base/notifications/classes/BxBaseModNotificationsInstaller.php
+++ modules/base/notifications/classes/BxBaseModNotificationsInstaller.php
@@ -15,32 +15,6 @@ class BxBaseModNotificationsInstaller extends BxBaseModGeneralInstaller
     {
         parent::__construct($aConfig);
     }
-
-	public function enable($aParams)
-    {
-    	$aResult = parent::enable($aParams);
-        if($aResult['result'])
-        	$this->_processHandlers('add_handlers');
-
-        return $aResult;
-    }
-
-    public function disable($aParams)
-    {
-    	$this->_processHandlers('delete_handlers');
-
-        return parent::disable($aParams);
-    }
-
-    protected function _processHandlers($sAction)
-    {
-    	$aModules = $this->oDb->getModulesBy(array('type' => 'all', 'active' => 1));
-	    foreach($aModules as $aModule) {
-	    	$aConfig = self::getModuleConfig($aModule);
-			if(!empty($aConfig['relations']) && is_array($aConfig['relations']) && in_array($this->_aConfig['name'], $aConfig['relations']))
-				BxDolService::call($this->_aConfig['name'], $sAction, array($aModule['uri']));
-		}
-    }
 }
 
 /** @} */
