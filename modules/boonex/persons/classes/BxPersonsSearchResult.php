f481d17676b3e3575798335788db446a4c2c1aa2
AntonLV <vlnotna@gmail.com>
2016-09-15 16:32:09 +0300

    Ticket #408 - Persons module

--- modules/boonex/persons/classes/BxPersonsSearchResult.php
+++ modules/boonex/persons/classes/BxPersonsSearchResult.php
@@ -49,7 +49,7 @@ class BxPersonsSearchResult extends BxBaseModProfileSearchResult
                     'joinFields' => array(),
                 ),
             ),
-            'paginate' => array('perPage' => 20, 'start' => 0),
+            'paginate' => array('perPage' => getParam('bx_persons_per_page_browse'), 'start' => 0),
             'sorting' => 'none',
             'rss' => array(
                 'title' => '',
