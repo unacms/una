ed8653b1772cb4c898d9ff089c9f8a93f7159177
Alex Trofimov <alex.trofimov@boonex.com>
2016-09-26 18:10:30 +0600

    Upgrade script from 9.0.0.B1 to 9.0.0.B2

---
+++ upgrade/files/9.0.0.B1-9.0.0.B2/files_delete.php
@@ -0,0 +1,9 @@
+<?php
+
+$aFilesDelete = array(
+    'modules/base/general/template/form_field_location.html',
+    'studio/template/bp_form_ghost_template.html',
+    'studio/template/designer_splash.html',
+    'template/css/editor.css',
+);
+
