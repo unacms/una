50c3f14af9c1ed8fc2a15cb2d0fc592401bcfa03
AntonLV <vlnotna@gmail.com>
2016-10-20 13:42:11 +0300

    Ticket #367

--- modules/boonex/forum/js/main.js
+++ modules/boonex/forum/js/main.js
@@ -26,21 +26,24 @@ BxForumMain.prototype.init = function() {
         return;
 
     var oFunction = function () {
+    	$('.bx-grid-table').each(function() {
+    		var oTable = $(this);
 
-        // calculate width of all table rows, except the one which contains messages preview
-        var iInnerWidth = $('.bx-grid-table').parent().innerWidth();
-        var iCalcWidth = 0;
-        $('.bx-grid-table tbody tr:first-child td').each(function () {
-            if (!$(this).find('.bx-forum-grid-preview').length)
-                iCalcWidth += $(this).outerWidth();
-        });
+	        // calculate width of all table cols, except the one which contains item's preview
+	        var iCalcWidth = 0;
+	        oTable.find('tbody tr:first-child td').each(function () {
+	            if(!$(this).find('.bx-forum-grid-preview').length)
+	            	iCalcWidth += $(this).outerWidth();
+	        });
 
-        // set width for messages previews
-        $('.bx-forum-grid-preview').each(function () {
-            var eWrapper = $(this).parent();
-            var w = iInnerWidth - iCalcWidth - parseInt(eWrapper.css('padding-left')) - parseInt(eWrapper.css('padding-right'));
-            $(this).width(w + 'px');
-        }); 
+	        // set width for items' previews
+	        var iInnerWidth = oTable.parent().innerWidth();
+	        oTable.find('.bx-forum-grid-preview').each(function () {
+	            var eWrapper = $(this).parent();
+	            var w = iInnerWidth - iCalcWidth - parseInt(eWrapper.css('padding-left')) - parseInt(eWrapper.css('padding-right'));
+	            $(this).width(w + 'px');
+	        }); 
+    	});
     };
 
     $(window).resize(function() {
