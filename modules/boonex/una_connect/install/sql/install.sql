821e3aae4eb6ef5f6c840142721c1859750fe745
Alex Trofimov <dev@boonex.com>
2016-11-04 16:38:49 +0600

    #485 Renaming Trident Connect to UNA Connect

--- modules/boonex/una_connect/install/sql/install.sql
+++ modules/boonex/una_connect/install/sql/install.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE `bx_tricon_accounts` (
+CREATE TABLE `bx_unacon_accounts` (
   `local_profile` int(10) unsigned NOT NULL,
   `remote_profile` int(10) unsigned NOT NULL,
   PRIMARY KEY (`local_profile`),
@@ -9,13 +9,13 @@ CREATE TABLE `bx_tricon_accounts` (
 -- Studio page and widget
 
 INSERT INTO `sys_std_pages`(`index`, `name`, `header`, `caption`, `icon`) VALUES
-(3, 'bx_tricon', '_bx_tricon', '_bx_tricon', 'bx_tricon@modules/boonex/trident_connect/|std-icon.svg');
+(3, 'bx_unacon', '_bx_unacon', '_bx_unacon', 'bx_unacon@modules/boonex/una_connect/|std-icon.svg');
 SET @iPageId = LAST_INSERT_ID();
 
 SET @iParentPageId = (SELECT `id` FROM `sys_std_pages` WHERE `name` = 'home');
 SET @iParentPageOrder = (SELECT MAX(`order`) FROM `sys_std_pages_widgets` WHERE `page_id` = @iParentPageId);
 INSERT INTO `sys_std_widgets` (`page_id`, `module`, `url`, `click`, `icon`, `caption`, `cnt_notices`, `cnt_actions`) VALUES
-(@iPageId, 'bx_tricon', '{url_studio}module.php?name=bx_tricon', '', 'bx_tricon@modules/boonex/trident_connect/|std-icon.svg', '_bx_tricon', '', 'a:4:{s:6:"module";s:6:"system";s:6:"method";s:11:"get_actions";s:6:"params";a:0:{}s:5:"class";s:18:"TemplStudioModules";}');
+(@iPageId, 'bx_unacon', '{url_studio}module.php?name=bx_unacon', '', 'bx_unacon@modules/boonex/una_connect/|std-icon.svg', '_bx_unacon', '', 'a:4:{s:6:"module";s:6:"system";s:6:"method";s:11:"get_actions";s:6:"params";a:0:{}s:5:"class";s:18:"TemplStudioModules";}');
 INSERT INTO `sys_std_pages_widgets` (`page_id`, `widget_id`, `order`) VALUES
 (@iParentPageId, LAST_INSERT_ID(), IF(ISNULL(@iParentPageOrder), 1, @iParentPageOrder + 1));
 
