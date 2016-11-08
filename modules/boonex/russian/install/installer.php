dcb7fb84e639894acaca0bb4c32e378f55e17589
Alex Trofimov <dev@boonex.com>
2016-11-08 17:08:12 +0600

    #489 Renaming: Russian language

--- modules/boonex/russian/install/installer.php
+++ modules/boonex/russian/install/installer.php
@@ -1,10 +1,10 @@
 <?php defined('BX_DOL') or die('hack attempt');
 /**
- * Copyright (c) BoonEx Pty Limited - http://www.boonex.com/
- * CC-BY License - http://creativecommons.org/licenses/by/3.0/
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
  *
  * @defgroup    Russian Russian language
- * @ingroup     TridentModules
+ * @ingroup     UnaModules
  *
  * @{
  */
