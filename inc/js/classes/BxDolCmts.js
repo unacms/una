7f8c9f33ee63245b6b17dfc8286378a123b48848
Alex Trofimov <dev@boonex.com>
2016-11-09 10:00:14 +0600

    #489 Renaming: inc

--- inc/js/classes/BxDolCmts.js
+++ inc/js/classes/BxDolCmts.js
@@ -1,8 +1,8 @@
 /**
- * Copyright (c) BoonEx Pty Limited - http://www.boonex.com/
- * CC-BY License - http://creativecommons.org/licenses/by/3.0/
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
  *
- * @defgroup    TridentCore Trident Core
+ * @defgroup    UnaCore UNA Core
  * @{
  */
 
