d7eb49f4d2a05cf3a48b083925a2f74cf9c5619f
Alex Trofimov <dev@boonex.com>
2016-11-09 13:32:26 +0600

    #489 Renaming: template folder

--- template/scripts/BxBaseMenuCustom.php
+++ template/scripts/BxBaseMenuCustom.php
@@ -1,9 +1,9 @@
 <?php defined('BX_DOL') or die('hack attempt');
 /**
- * Copyright (c) BoonEx Pty Limited - http://www.boonex.com/
- * CC-BY License - http://creativecommons.org/licenses/by/3.0/
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
  *
- * @defgroup    TridentCore Trident Core
+ * @defgroup    UnaCore UNA Core
  *
  * @{
  */
