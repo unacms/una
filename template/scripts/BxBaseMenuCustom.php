04e28359eb24aa2b6b2a6f760bae51c8df91bf4e
Alex Trofimov <dev@boonex.com>
2016-11-10 07:42:54 +0600

    #489 Docs corrections

--- template/scripts/BxBaseMenuCustom.php
+++ template/scripts/BxBaseMenuCustom.php
@@ -3,7 +3,7 @@
  * Copyright (c) UNA, Inc - https://una.io
  * MIT License - https://opensource.org/licenses/MIT
  *
- * @defgroup    UnaCore UNA Core
+ * @defgroup    UnaCoreBaseRepresentation UNA Core Base Representation Classes
  *
  * @{
  */
