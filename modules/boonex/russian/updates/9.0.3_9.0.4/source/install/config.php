dafeeaa29f64999d257d55d5c1ae84ea0144b4c2
AntonLV <vlnotna@gmail.com>
2016-11-16 07:49:59 +0300

    Modules updates.

--- modules/boonex/russian/updates/9.0.3_9.0.4/source/install/config.php
+++ modules/boonex/russian/updates/9.0.3_9.0.4/source/install/config.php
@@ -17,7 +17,7 @@ $aConfig = array(
     'name' => 'bx_ru',
     'title' => 'Russian',
     'note' => 'Language file',
-    'version' => '9.0.5.DEV',
+    'version' => '9.0.4',
     'vendor' => 'Boonex',
 	'help_url' => 'http://feed.boonex.com/?section={module_name}',
 
