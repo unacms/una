1f754511a3aac137f513650111c25f9789215a94
AntonLV <vlnotna@gmail.com>
2016-11-25 12:39:27 +0300

    Ticket #478

--- modules/base/general/classes/BxBaseModGeneralModule.php
+++ modules/base/general/classes/BxBaseModGeneralModule.php
@@ -761,14 +761,35 @@ class BxBaseModGeneralModule extends BxDolModule
         $CNF = &$this->_oConfig->CNF;
 
         $sImage = '';
-        if (isset($CNF['FIELD_THUMB']) && isset($aContentInfo[$CNF['FIELD_THUMB']]) && $aContentInfo[$CNF['FIELD_THUMB']]) {
-        	
-        	$oTranscoder = BxDolTranscoderImage::getObjectInstance($CNF['OBJECT_IMAGES_TRANSCODER_GALLERY']);
-        	if($oTranscoder)
-                $sImage = $oTranscoder->getFileUrl($aContentInfo[$CNF['FIELD_THUMB']]);
+        if(isset($CNF['FIELD_COVER']) && isset($aContentInfo[$CNF['FIELD_COVER']]) && $aContentInfo[$CNF['FIELD_COVER']]) {
+            if(!empty($CNF['OBJECT_IMAGES_TRANSCODER_COVER'])) {
+                $oTranscoder = BxDolTranscoderImage::getObjectInstance($CNF['OBJECT_IMAGES_TRANSCODER_COVER']);
+            	if($oTranscoder)
+                    $sImage = $oTranscoder->getFileUrl($aContentInfo[$CNF['FIELD_COVER']]);
+            }
+
+            if($sImage == '' && !empty($CNF['OBJECT_IMAGES_TRANSCODER_THUMB'])) {
+                $oTranscoder = BxDolTranscoderImage::getObjectInstance($CNF['OBJECT_IMAGES_TRANSCODER_THUMB']);
+            	if($oTranscoder)
+                    $sImage = $oTranscoder->getFileUrl($aContentInfo[$CNF['FIELD_COVER']]);
+            }
         }
 
-        if (empty($sImage))
+        if($sImage == '' && isset($CNF['FIELD_THUMB']) && isset($aContentInfo[$CNF['FIELD_THUMB']]) && $aContentInfo[$CNF['FIELD_THUMB']]) {
+            if(!empty($CNF['OBJECT_IMAGES_TRANSCODER_THUMB'])) {
+                $oTranscoder = BxDolTranscoderImage::getObjectInstance($CNF['OBJECT_IMAGES_TRANSCODER_THUMB']);
+            	if($oTranscoder)
+                    $sImage = $oTranscoder->getFileUrl($aContentInfo[$CNF['FIELD_THUMB']]);
+            }
+
+            if($sImage == '' && !empty($CNF['OBJECT_IMAGES_TRANSCODER_GALLERY'])) {
+            	$oTranscoder = BxDolTranscoderImage::getObjectInstance($CNF['OBJECT_IMAGES_TRANSCODER_GALLERY']);
+            	if($oTranscoder)
+                    $sImage = $oTranscoder->getFileUrl($aContentInfo[$CNF['FIELD_THUMB']]);
+            }
+        }
+
+        if(empty($sImage))
             return array();
 
         return array(
