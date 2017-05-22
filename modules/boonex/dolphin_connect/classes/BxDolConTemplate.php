e5abbe29e4da5ccf937f352560e97dba5e51ec01
Alex Trofimov <dev@boonex.com>
2017-05-22 12:42:41 +0600

    #668 Dolphin Connect module

---
+++ modules/boonex/dolphin_connect/classes/BxDolConTemplate.php
@@ -0,0 +1,20 @@
+<?php defined('BX_DOL') or die('hack attempt');
+/**
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
+ *
+ * @defgroup    UnaConnect Dolphin Connect
+ * @ingroup     UnaModules
+ *
+ * @{
+ */
+
+class BxDolConTemplate extends BxBaseModConnectTemplate
+{
+    function __construct(&$oConfig, &$oDb)
+    {
+        parent::__construct($oConfig, $oDb);
+    }
+}
+
+/** @} */
