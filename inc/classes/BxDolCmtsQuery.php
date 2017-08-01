1bd234d531cfac026b75a0a6ce5631e0a567c233
AntonLV <vlnotna@gmail.com>
2017-08-01 11:50:49 +0300

    Ticket #744

--- inc/classes/BxDolCmtsQuery.php
+++ inc/classes/BxDolCmtsQuery.php
@@ -238,6 +238,17 @@ class BxDolCmtsQuery extends BxDolDb
                             $sSearchValue = " IN (" . $this->implode_escape($aSearchParam['value']) . ")";
                             break;
 
+                        case 'and':
+                            $iResult = 0;
+                            if (is_array($aSearchParam['value']))
+                                foreach ($aSearchParam['value'] as $iValue)
+                                    $iResult |= pow (2, $iValue - 1);
+                            else 
+                                $iResult = (int)$aSearchParam['value'];
+
+                            $sSearchValue = " & " . $iResult . "";
+                            break;
+
                         default:
                              $sSearchValue = " " . $aSearchParam['operator'] . " :" . $sSearchParam;
                              $aMethod['params'][1][$sSearchParam] = $aSearchParam['value'];                             
