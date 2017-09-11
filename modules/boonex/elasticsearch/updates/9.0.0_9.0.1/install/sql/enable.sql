14573619f349a877d19ee0e784ab3baf9bdafaca
AntonLV <vlnotna@gmail.com>
2017-09-11 17:10:29 +0300

    Modules updates.

--- modules/boonex/elasticsearch/updates/9.0.0_9.0.1/install/sql/enable.sql
+++ modules/boonex/elasticsearch/updates/9.0.0_9.0.1/install/sql/enable.sql
@@ -1,2 +1,2 @@
 -- SETTINGS
-UPDATE `sys_options` SET `value`='' WHERE `name`='bx_elasticsearch_api_url';
+UPDATE `sys_options` SET `value`='' WHERE `name`='bx_elasticsearch_api_url' AND `value`='http://45.56.80.210:9200';
