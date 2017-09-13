f425f3e2f77eacdb1e33a0bfa6f7cb6132ca4ac4
AntonLV <vlnotna@gmail.com>
2017-09-13 14:53:52 +0300

    After release version update.

--- modules/boonex/decorous/install/config.php
+++ modules/boonex/decorous/install/config.php
@@ -17,7 +17,7 @@ $aConfig = array(
     'name' => 'bx_decorous',
     'title' => 'Decorous',
     'note' => 'Design template',
-    'version' => '9.0.1.DEV',
+    'version' => '9.0.2.DEV',
     'vendor' => 'Boonex',
 	'help_url' => 'http://feed.boonex.com/?section={module_name}',
 
--- modules/boonex/payment/install/config.php
+++ modules/boonex/payment/install/config.php
@@ -17,7 +17,7 @@ $aConfig = array(
     'name' => 'bx_payment',
     'title' => 'Payment',
     'note' => 'Payment module.',
-    'version' => '9.0.5.DEV',
+    'version' => '9.0.6.DEV',
     'vendor' => 'Boonex',
     'help_url' => 'http://feed.boonex.com/?section={module_name}',
 
