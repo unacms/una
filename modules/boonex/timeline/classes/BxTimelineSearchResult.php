7715c30d4fb39e55d2f7417beb0bc40c7ef03314
AntonLV <vlnotna@gmail.com>
2017-09-18 09:49:14 +0300

    Ticket #880

--- modules/boonex/timeline/classes/BxTimelineSearchResult.php
+++ modules/boonex/timeline/classes/BxTimelineSearchResult.php
@@ -22,7 +22,7 @@ class BxTimelineSearchResult extends BxBaseModNotificationsSearchResult
             'title' => _t('_bx_timeline_page_title_browse'),
             'table' => 'bx_timeline_events',
             'ownFields' => array('id', 'owner_id', 'type', 'action', 'object_id', 'object_privacy_view', 'content', 'title', 'description', 'views', 'rate', 'votes', 'comments', 'reports', 'reposts', 'date', 'active', 'hidden', 'pinned'),
-            'searchFields' => array('description'),
+            'searchFields' => array('title', 'description'),
             'restriction' => array(
                 'internal' => array('value' => 'timeline_common_post', 'field' => 'type', 'operator' => '='),
                 'active' => array('value' => '1', 'field' => 'active', 'operator' => '='),
