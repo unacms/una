35e009fc0f51845ddcb1bae1134d2f68be88f7de
AntonLV <vlnotna@gmail.com>
2017-10-23 09:15:38 +0300

    Ticket #934

--- template/scripts/BxBaseMenu.php
+++ template/scripts/BxBaseMenu.php
@@ -32,9 +32,6 @@ class BxBaseMenu extends BxDolMenu
      */
     public function getCode ()
     {
-        if(!$this->isVisible())
-            return '';
-
         $sMenuTitle = isset($this->_aObject['title']) ? _t($this->_aObject['title']) : 'Menu-' . rand(0, PHP_INT_MAX);
         if (isset($GLOBALS['bx_profiler'])) $GLOBALS['bx_profiler']->beginMenu($sMenuTitle);
 
