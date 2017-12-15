652450f22bb714973ba4ffe1421f18e7e0a1032c
Alex Trofimov <dev@boonex.com>
2017-12-15 12:36:33 +0600

    #644  "Sites" module has been deleted

--- modules/boonex/sites/api/PPBootStrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('PP_CONFIG_PATH', BX_SITES_PP_DIRECTORY_PATH_API);
-
-require 'PPAutoloader.php';
-PPAutoloader::register();
--- modules/boonex/sites/api/lib/handlers/IPPHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-interface IPPHandler {
-	/**
-	 * 
-	 * @param PPHttpConfig $httpConfig
-	 * @param PPRequest $request 
-	 */
-	public function handle($httpConfig, $request);
-}
