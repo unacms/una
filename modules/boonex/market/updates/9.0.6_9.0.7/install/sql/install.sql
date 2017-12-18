84a68a7a0bc80fe9aed297be5d175600c7392557
AntonLV <vlnotna@gmail.com>
2017-12-18 16:03:33 +0300

    Modules updates.

---
+++ modules/boonex/market/updates/9.0.6_9.0.7/install/sql/install.sql
@@ -0,0 +1,1 @@
+UPDATE sys_modules SET help_url = 'http://feed.una.io/?section={module_name}' WHERE name = 'bx_market' LIMIT 1;
---
+++ modules/boonex/shopify/updates/9.0.1_9.0.2/install/sql/install.sql
@@ -0,0 +1,1 @@
+UPDATE sys_modules SET help_url = 'http://feed.una.io/?section={module_name}' WHERE name = 'bx_shopify' LIMIT 1;
