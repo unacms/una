7a3f7dbc1e7849b2aa935a9de304e3517df7f09f
AntonLV <vlnotna@gmail.com>
2018-01-26 11:34:04 +0300

    Ticket #1072

--- modules/boonex/decorous/data/template/system/scripts/BxTemplFunctions.php
+++ modules/boonex/decorous/data/template/system/scripts/BxTemplFunctions.php
@@ -13,6 +13,25 @@ class BxTemplFunctions extends BxBaseFunctions
     {
         parent::__construct($oTemplate);
     }
+
+    protected function getInjFooterPopupMenus() 
+    {
+        $sContent = '';
+
+        $oSearch = new BxTemplSearch();
+        $oSearch->setLiveSearch(true);
+        $sContent .= $this->_oTemplate->parsePageByName('search.html', array(
+            'search_form' => $oSearch->getForm(BX_DB_CONTENT_ONLY),
+            'results' => $oSearch->getResultsContainer(),
+        ));
+
+        if(isLogged()) {
+            $sContent .= $this->_oTemplate->getMenu ('sys_add_content');
+            $sContent .= $this->_oTemplate->getMenu ('sys_account_popup');
+        }
+
+        return $sContent;
+    }
 }
 
 /** @} */
