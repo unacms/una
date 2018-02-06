84d0ad818bbbe762a1bb915a0d0cc5ef4d4ea2f2
AntonLV <vlnotna@gmail.com>
2018-02-06 13:13:47 +0300

    Modules updates.

--- modules/boonex/acl/updates/9.0.6_9.0.7/install/sql/install.sql
+++ modules/boonex/acl/updates/9.0.6_9.0.7/install/sql/install.sql
@@ -1,3 +1,15 @@
+-- TABLE: bx_acl_level_prices
+
+ALTER TABLE `bx_acl_level_prices` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE `bx_acl_level_prices` CHANGE `name` `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `bx_acl_level_prices` CHANGE `period_unit` `period_unit` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+REPAIR TABLE `bx_acl_level_prices`;
+OPTIMIZE TABLE `bx_acl_level_prices`;
+
+
+-- FORMS
 DELETE FROM `sys_form_display_inputs` WHERE `display_name`='bx_acl_price_edit';
 INSERT INTO `sys_form_display_inputs` (`display_name`, `input_name`, `visible_for_levels`, `active`, `order`) VALUES
 ('bx_acl_price_edit', 'id', 2147483647, 1, 1),
---
+++ modules/boonex/convos/updates/9.0.6_9.0.7/install/sql/enable.sql
@@ -0,0 +1,2 @@
+-- COMMENTS
+UPDATE `sys_objects_cmts` SET `Nl2br`='0' WHERE `Name`='bx_convos';
