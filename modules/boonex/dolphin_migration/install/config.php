197c15460beff234a3e9394756f77cba414ea31d
AntonLV <vlnotna@gmail.com>
2018-06-13 16:21:11 +0300

    Dolphin Migration update.

--- modules/boonex/dolphin_migration/install/config.php
+++ modules/boonex/dolphin_migration/install/config.php
@@ -17,7 +17,7 @@ $aConfig = array(
     'name' => 'bx_dolphin_migration',
     'title' => 'Dolphin Migration',
     'note' => 'Migration Tool',
-    'version' => '9.0.1.DEV',
+    'version' => '9.0.2.DEV',
     'vendor' => 'Boonex',
 	'help_url' => 'http://feed.una.io/?section={module_name}',
 
