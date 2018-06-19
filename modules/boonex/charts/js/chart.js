abe5484cd4332507fabd98fc70ac27d7832d974a
RomanL <roman.lesnikov@gmail.com>
2018-06-19 17:33:50 +0300

    #1252 improvements

--- modules/boonex/charts/js/chart.js
+++ modules/boonex/charts/js/chart.js
@@ -13,7 +13,7 @@ function BxCharts(oOptions) {
     this._sChartName = oOptions.sChartName == undefined ? '' : oOptions.sChartName;
     this._sActionsUri = oOptions.sActionUri;
     this._sKeyGraph = 'bx_chart_growth_graph_' + this._sChartName;
-    this._oChartOptionsDefault = {};
+    this._oChartOptionsDefault = {  };
     var $this = this;
     $(document).ready(function () {
         $this.loadData();
@@ -42,9 +42,18 @@ BxCharts.prototype.loadData = function () {
     	        oGraphErr.html(oData.error).show();
     	        return;
     	    }
+
+    	    if (oData.links) {
+    	        Chart.defaults.doughnut.legend.onClick = function (e, legendItem) {
+    	            var iIndex = $this._oChart.data.datasets[0].backgroundColor.indexOf(legendItem.fillStyle);
+    	            if (iIndex && oData.links[iIndex]) {
+    	                location.href = oData.links[iIndex];
+    	            }
+    	        };
+    	    }
+
     	    var oDataForChart = oData.data || false;
     	    var oChartOptions = oData.options || {};
-
     	    if ($this._oChart)
     	        $this._oChart.destroy();
     	   
@@ -57,11 +66,13 @@ BxCharts.prototype.loadData = function () {
     	    if (oData.links) {
     	        oGraph.click(function (evt) {
     	            var activePoint = $this._oChart.getElementAtEvent(evt)[0];
-    	            var data = activePoint._chart.data;
-    	            var datasetIndex = activePoint._datasetIndex;
-    	            var label = data.datasets[datasetIndex].label;
-    	            var value = data.datasets[datasetIndex].data[activePoint._index];
-    	            location.href = oData.links[activePoint._index];
+    	            if (activePoint) {
+    	                var data = activePoint._chart.data;
+    	                var datasetIndex = activePoint._datasetIndex;
+    	                var label = data.datasets[datasetIndex].label;
+    	                var value = data.datasets[datasetIndex].data[activePoint._index];
+    	                location.href = oData.links[activePoint._index];
+    	            }
     	        });
     	    }
 
