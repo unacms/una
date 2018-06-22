a2dc2f76b59ff5fc03ed6185503a3f54ce4d833d
Alex Trofimov <dev@boonex.com>
2018-06-22 21:36:21 +1000

    #1349 FontAwesome Pro Module

---
+++ modules/boonex/fontawesome/install/installer.php
@@ -0,0 +1,20 @@
+<?php defined('BX_DOL') or die('hack attempt');
+/**
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
+ *
+ * @defgroup    FontAwesome Font Awesome Pro integration
+ * @ingroup     UnaModules
+ *
+ * @{
+ */
+
+class BxFontAwesomeInstaller extends BxDolStudioInstaller
+{
+    function __construct($aConfig)
+    {
+        parent::__construct($aConfig);
+    }
+}
+
+/** @} */
