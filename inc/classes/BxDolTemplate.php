06e06983274b8d2276b154699b7cd16d4e5b1c8a
AntonLV <vlnotna@gmail.com>
2018-06-27 15:18:43 +0300

    Ticket #255 - changeable menu item addons.

--- inc/classes/BxDolTemplate.php
+++ inc/classes/BxDolTemplate.php
@@ -247,7 +247,7 @@ class BxDolTemplate extends BxDolFactory implements iBxDolSingleton
         $this->_sFolderCss = 'css/';
         $this->_sFolderImages = 'images/';
         $this->_sFolderIcons = 'images/icons/';
-        $this->_aTemplates = array('html_tags', 'menu_item_addon');
+        $this->_aTemplates = array('html_tags', 'menu_item_addon', 'menu_item_addon_small', 'menu_item_addon_middle');
 
         $this->addLocation('system', $this->_sRootPath, $this->_sRootUrl);
 
