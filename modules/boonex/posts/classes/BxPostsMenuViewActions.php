8fe2330fe5603ca68a8434786b0275d5022c860e
AntonLV <vlnotna@gmail.com>
2018-08-21 09:47:22 +0300

    Ticket #1524 - Usage in Posts module.

---
+++ modules/boonex/posts/classes/BxPostsMenuViewActions.php
@@ -0,0 +1,25 @@
+<?php defined('BX_DOL') or die('hack attempt');
+/**
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
+ *
+ * @defgroup    Posts Posts
+ * @ingroup     UnaModules
+ *
+ * @{
+ */
+
+/**
+ * View entry social actions menu
+ */
+class BxPostsMenuViewActions extends BxBaseModTextMenuViewActions
+{
+    public function __construct($aObject, $oTemplate = false)
+    {
+        $this->_sModule = 'bx_posts';
+
+        parent::__construct($aObject, $oTemplate);
+    }
+}
+
+/** @} */
