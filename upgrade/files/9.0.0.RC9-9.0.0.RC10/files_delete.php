65d0f0453a5ab1fe064712cb307cf3e9ef588d81
Alex Trofimov <dev@boonex.com>
2018-09-20 14:55:05 +1000

    Upgrade script from 9.0.0.RC9 to 9.0.0.RC10

---
+++ upgrade/files/9.0.0.RC9-9.0.0.RC10/files_delete.php
@@ -0,0 +1,11 @@
+<?php
+
+$aFilesDelete = array(
+    'modules/base/payment/template/link.html',
+    'plugins/stripe/data/test.png',
+    'plugins/stripe/lib/AttachedObject.php',
+    'plugins/stripe/lib/ExternalAccount.php',
+    'plugins/stripe/lib/JsonSerializable.php',
+    'plugins_public/spin.min.js',
+);
+
