62150d16cd84c2cfdab9bfdbc86cb725099fe791
AlexeyUna <ad@una.io>
2018-09-25 11:32:07 +0300

    Dolphin Migration Tool update script corrections

--- modules/boonex/dolphin_migration/updates/9.0.2_9.0.3/install/sql/enable.sql
+++ modules/boonex/dolphin_migration/updates/9.0.2_9.0.3/install/sql/enable.sql
@@ -1,6 +1,8 @@
 SET @sName = 'bx_dolphin_migration';
 
-SET @iCategId = (SELECT `id` FROM `sys_options_categories` WHERE `Caption` = '_bx_dolphin_migration_wgt_cpt' LIMIT 1);
+SET @iCategId = (SELECT `id` FROM `sys_options_categories` WHERE `name` = @sName LIMIT 1);
+
+DELETE FROM `sys_options`  WHERE `name` IN('bx_dolphin_migration_overwrite', 'bx_dolphin_migration_use_nickname', 'bx_dolphin_migration_empty_albums');
 
 INSERT INTO `sys_options` (`name`, `value`, `category_id`, `caption`, `type`, `check`, `check_error`, `extra`, `order`) VALUES
 ('bx_dolphin_migration_overwrite', '', @iCategId, '_bx_dolphin_migration_cpt_overwrite', 'checkbox', '', '', '', 0),
