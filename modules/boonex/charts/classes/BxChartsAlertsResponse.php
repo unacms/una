f2e416e22d08951b49f36e00c3f14bd8e921582e
RomanL <roman.lesnikov@gmail.com>
2018-10-01 12:21:47 +0300

    #1644

--- modules/boonex/charts/classes/BxChartsAlertsResponse.php
+++ modules/boonex/charts/classes/BxChartsAlertsResponse.php
@@ -18,8 +18,10 @@ class BxChartsAlertsResponse extends BxDolAlertsResponse
     
     public function response($oAlert)
     {
-        $oModule = BxDolModule::getInstance('bx_charts');
-        BxDolCronQuery::getInstance()->addTransientJobClass('bx_charts_cron_transient', 'BxChartsCron', "modules/" . $oModule->_oConfig->getDirectory() . 'classes/' . $oModule->_oConfig->getClassPrefix() . 'Cron.php');
+        if (isset($oAlert->aExtras['category']) && isset($oAlert->aExtras['category']['type_name']) && isset($oAlert->aExtras['category']['type_group']) && $oAlert->aExtras['category']['type_name'] == 'bx_charts' && $oAlert->aExtras['category']['type_group'] == 'modules'){
+            $oModule = BxDolModule::getInstance('bx_charts');
+            BxDolCronQuery::getInstance()->addTransientJobClass('bx_charts_cron_transient', 'BxChartsCron', "modules/" . $oModule->_oConfig->getDirectory() . 'classes/' . $oModule->_oConfig->getClassPrefix() . 'Cron.php');
+        }
     }
 }
 
