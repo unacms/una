20941b9ddb03c764d5ee365b75de201eae44cea9
AntonLV <vlnotna@gmail.com>
2018-10-02 09:09:14 +0300

    Modules updates.

--- modules/boonex/notifications/updates/9.0.10_9.0.11/source/install/config.php
+++ modules/boonex/notifications/updates/9.0.10_9.0.11/source/install/config.php
@@ -17,12 +17,12 @@ $aConfig = array(
     'name' => 'bx_notifications',
     'title' => 'Notifications',
     'note' => 'Notifications module.',
-    'version' => '9.0.12.DEV',
+    'version' => '9.0.11',
     'vendor' => 'BoonEx',
 	'help_url' => 'http://feed.una.io/?section={module_name}',
 
     'compatible_with' => array(
-        '9.0.x'
+        '9.0.0-RC10'
     ),
 
     /**
