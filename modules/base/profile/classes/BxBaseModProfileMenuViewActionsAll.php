04ff38d60c5d42e961880b0d73c4af3257a0f270
AntonLV <vlnotna@gmail.com>
2018-10-04 15:01:16 +0300

    Ticket #1648

--- modules/base/profile/classes/BxBaseModProfileMenuViewActionsAll.php
+++ modules/base/profile/classes/BxBaseModProfileMenuViewActionsAll.php
@@ -14,12 +14,31 @@
  */
 class BxBaseModProfileMenuViewActionsAll extends BxBaseModGeneralMenuViewActions
 {
+    protected $_oProfile;
+    protected $_aProfileInfo;
+
     public function __construct($aObject, $oTemplate = false)
     {
         parent::__construct($aObject, $oTemplate);
 
         if(empty($this->_iContentId) && bx_get('profile_id') !== false)
             $this->setContentId(BxDolProfile::getInstance(bx_process_input(bx_get('profile_id'), BX_DATA_INT))->getContentId());
+    }
+    
+    public function setContentId($iContentId)
+    {
+        parent::setContentId($iContentId);
+
+        $this->_oProfile = BxDolProfile::getInstanceByContentAndType($this->_iContentId, $this->_sModule);
+        if(!$this->_oProfile) 
+            return;
+
+        $this->_aProfileInfo = $this->_oProfile->getInfo();     
+
+        $this->addMarkers($this->_aProfileInfo);
+        $this->addMarkers(array(
+            'profile_id' => $this->_oProfile->id()
+        ));
     }
 
     protected function _getMenuItemProfileFriendAdd($aItem)
