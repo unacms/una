4a59a95e419c92d1a28405ff18c6477fc08e7f8f
Alex Trofimov <dev@boonex.com>
2018-10-10 16:40:50 +1100

    #1676 Drupal connect

---
+++ modules/boonex/drupal_connect/install/installer.php
@@ -0,0 +1,20 @@
+<?php defined('BX_DOL') or die('hack attempt');
+/**
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
+ *
+ * @defgroup    DrupalConnect Drupal Connect
+ * @ingroup     UnaModules
+ *
+ * @{
+ */
+
+class BxDrupalInstaller extends BxDolStudioInstaller
+{
+    function __construct($aConfig)
+    {
+        parent::__construct($aConfig);
+    }
+}
+
+/** @} */
