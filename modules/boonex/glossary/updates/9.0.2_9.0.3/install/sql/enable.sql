ce0d7b465a5f8ac8ac30d866e4bec37667344dc7
AntonLV <vlnotna@gmail.com>
2018-10-17 14:20:38 +0300

    Modules updates.

---
+++ modules/boonex/glossary/updates/9.0.2_9.0.3/install/sql/enable.sql
@@ -0,0 +1,2 @@
+-- PAGES
+UPDATE `sys_objects_page` SET `layout_id`='5' WHERE `object`='bx_glossary_home';
---
+++ modules/boonex/quoteofday/updates/9.0.3_9.0.4/install/sql/enable.sql
@@ -0,0 +1,2 @@
+-- GRIDS
+UPDATE `sys_grid_fields` SET `chars_limit`='100' WHERE `object`='bx_quoteofday_internal' AND `name`='text';
