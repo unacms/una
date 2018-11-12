b148ed145f59116c2b2aa49bc0d63e97b19d4528
AntonLV <vlnotna@gmail.com>
2018-11-12 10:04:02 +0300

    Modules updates.

--- modules/boonex/oauth2/updates/9.0.8_9.0.9/install/config.php
+++ modules/boonex/oauth2/updates/9.0.8_9.0.9/install/config.php
@@ -33,7 +33,7 @@ $aConfig = array(
      * Installation/Uninstallation Section.
      */
     'install' => array(
-		'execute_sql' => 0
+		'execute_sql' => 0,
         'update_files' => 1,
         'update_languages' => 0,
 		'clear_db_cache' => 0,
