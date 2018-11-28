1061ff40c0900a4ca0851b123823ad5490b952b2
AntonLV <vlnotna@gmail.com>
2018-11-28 12:33:48 +0300

    Modules updates.

---
+++ modules/boonex/persons/updates/9.0.11_9.0.12/source/classes/BxPersonsVote.php
@@ -0,0 +1,22 @@
+<?php defined('BX_DOL') or die('hack attempt');
+/**
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
+ *
+ * @defgroup    Persons Persons
+ * @ingroup     UnaModules
+ *
+ * @{
+ */
+
+class BxPersonsVote extends BxBaseModProfileVote
+{
+    function __construct($sSystem, $iId, $iInit = 1)
+    {
+    	$this->_sModule = 'bx_persons';
+
+        parent::__construct($sSystem, $iId, $iInit);        
+    }
+}
+
+/** @} */
