0f2c26793845547efd8e8234c42157c9ac108fb2
AntonLV <vlnotna@gmail.com>
2019-04-04 16:23:08 +0300

    Ticket #2051

--- modules/boonex/posts/classes/BxPostsFormEntry.php
+++ modules/boonex/posts/classes/BxPostsFormEntry.php
@@ -168,6 +168,17 @@ class BxPostsFormEntry extends BxBaseModTextFormEntry
             ),
         );
     }
+
+    protected function _getPhotoGhostTmplVars($aContentInfo = array())
+    {
+    	$CNF = &$this->_oModule->_oConfig->CNF;
+
+    	return array (
+            'name' => $this->aInputs[$CNF['FIELD_PHOTO']]['name'],
+            'content_id' => (int)$this->aInputs[$CNF['FIELD_PHOTO']]['content_id'],
+            'editor_id' => isset($CNF['FIELD_TEXT_ID']) ? $CNF['FIELD_TEXT_ID'] : ''
+    	);
+    }
 }
 
 /** @} */
