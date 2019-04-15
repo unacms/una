43cc6b212d5c1ced3b7977c1067bbabae08c33ff
Alex Trofimov <dev@boonex.com>
2019-04-15 16:52:08 +1000

    #2005 Mobile Apps Module

---
+++ modules/boonex/mobile_apps/classes/BxMobileAppsAlerts.php
@@ -0,0 +1,38 @@
+<?php defined('BX_DOL') or die('hack attempt');
+/**
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
+ *
+ * @defgroup    MobileApps Mobile Apps
+ * @ingroup     UnaModules
+ *
+ * @{
+ */
+
+class BxMobileAppsAlerts extends BxDolAlertsResponse
+{
+    protected $_oModule;
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function response($o)
+    {
+        if ('system' == $o->sUnit && 'page_output' == $o->sAction && 'sys_home' == $o->aExtras['page_name'] && !isLogged() && false !== strpos($_SERVER['HTTP_USER_AGENT'], 'UNAMobileApp')) {
+
+            // require_once(BX_DIRECTORY_PATH_INC . "design.inc.php");
+    
+            $sLoginForm = BxDolService::call('system', 'login_form', array('ajax_form'), 'TemplServiceLogin');
+
+            $oTemplate = BxDolTemplate::getInstance();
+            $oTemplate->setPageNameIndex (BX_PAGE_DEFAULT);
+            $oTemplate->setPageContent ('page_main_code', DesignBoxContent('', $sLoginForm, BX_DB_PADDING_NO_CAPTION)); 
+            $oTemplate->getPageCode();
+            
+            exit;
+        }
+    }    
+}
+
+/** @} */
