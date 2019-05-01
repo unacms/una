5c716efceb7044a5ff5162a6445a46655663647a
AlexeyUna <ad@una.io>
2019-05-01 16:49:21 +0300

    Migration Dolphin to UNA update script

--- modules/boonex/dolphin_migration/updates/9.0.5_9.0.6/source/install/config.php
+++ modules/boonex/dolphin_migration/updates/9.0.5_9.0.6/source/install/config.php
@@ -17,12 +17,12 @@ $aConfig = array(
     'name' => 'bx_dolphin_migration',
     'title' => 'Dolphin Migration',
     'note' => 'Migration Tool',
-    'version' => '9.0.6.DEV',
+    'version' => '9.0.6',
     'vendor' => 'Boonex',
 	'help_url' => 'http://feed.una.io/?section={module_name}',
 
     'compatible_with' => array(
-        '9.0.x'
+        '9.0.0'
     ),
 
     /**
