9045982200c704b6e0b693becd5a7ce85a242c39
AntonLV <vlnotna@gmail.com>
2019-05-08 14:11:52 +0300

    Ticket #2105 - Get rid of dynamic Bars and Plus icons.

--- modules/boonex/decorous/data/template/system/scripts/BxTemplMenuToolbar.php
+++ modules/boonex/decorous/data/template/system/scripts/BxTemplMenuToolbar.php
@@ -12,9 +12,45 @@
  */
 class BxTemplMenuToolbar extends BxBaseMenuToolbar
 {
+    protected $_bSiteToolbar;
+
     public function __construct ($aObject, $oTemplate = false)
     {
         parent::__construct ($aObject, $oTemplate);
+
+        $this->_bSiteToolbar = $this->_sObject == 'sys_toolbar_site';
+    }
+
+    protected function _getMenuItem ($a)
+    {
+    	//--- Hide 'Search' from Site Toolbar
+        if($this->_bSiteToolbar && in_array($a['name'], array('search')))
+            return false;
+
+        $a = BxTemplMenu::_getMenuItem ($a);
+        if($a === false)
+            return $a;
+
+        $a['bx_if:unit'] = array(
+            'condition' => false,
+            'content' => array()
+        );
+
+        $a['class_add_a'] = '';
+        switch ($a['name']) {
+            case 'bx_lucid_search':
+                $a['class_add'] = 'bx-def-media-phone-hide';
+                break;
+
+            case 'main-menu':
+                $a['class_add'] = ' bx-def-media-desktop-hide bx-def-media-tablet-hide';
+                $a['class_add_a'] = ' cd-nav-trigger';
+                $a['link'] = 'javascript:void(0)';
+                $a['onclick'] = '';
+                break;
+        }
+
+        return $a;
     }
 }
 
