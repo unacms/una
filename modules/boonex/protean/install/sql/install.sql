f7ef378a24c39a7ff7b41df586bf5f7df351bf47
AntonLV <vlnotna@gmail.com>
2019-05-17 13:31:49 +0300

    Ticket #2096 - Dark mix.

--- modules/boonex/protean/install/sql/install.sql
+++ modules/boonex/protean/install/sql/install.sql
@@ -1080,7 +1080,7 @@ INSERT INTO `sys_options_mixes2options` (`option`, `mix_id`, `value`) VALUES
 ('bx_protean_menu_page_border_size', @iMixId, '1px'),
 ('bx_protean_menu_page_content_padding', @iMixId, '0px'),
 ('bx_protean_menu_page_font_color', @iMixId, 'rgba(68, 136, 255, 1)'),
-('bx_protean_menu_page_font_color_active', @iMixId, 'rgba(51, 68, 85, 1)'),
+('bx_protean_menu_page_font_color_active', @iMixId, 'rgba(252, 253, 254, 1)'),
 ('bx_protean_menu_page_font_color_hover', @iMixId, 'rgba(68, 136, 255, 0.8)'),
 ('bx_protean_menu_page_font_family', @iMixId, '\"Helvetica Neue\", sans-serif'),
 ('bx_protean_menu_page_font_shadow', @iMixId, 'none'),
@@ -1123,7 +1123,7 @@ INSERT INTO `sys_options_mixes2options` (`option`, `mix_id`, `value`) VALUES
 ('bx_protean_site_logo_alt', @iMixId, ''),
 ('bx_protean_site_logo_height', @iMixId, '48'),
 ('bx_protean_site_logo_width', @iMixId, '240'),
-('bx_protean_styles_custom', @iMixId, 'div.bx-base-pofile-cover {\r\nborder: 1px solid rgba(136, 153, 170, 0.3);\r\nborder-bottom-width: 0;\r\n}\r\n\r\n\r\ndiv.bx-market-unit-cover div.bx-base-text-unit-no-thumb {\r\nborder-width: 0px;\r\n}\r\n\r\n.sys-cpf-header,\r\n.sys-cpf-title,\r\n.sys-cpf-submenu-cnt ul li {\r\nborder-bottom-color: rgba(136, 153, 170, 0.3);\r\n}\r\n\r\n.sys-cpf-submenu-cnt ul li.bx-menu-inter-act {\r\nborder-bottom-color: rgb(17, 34, 51);\r\n}\r\n\r\n\r\n.bx-def-font {\r\nline-height:18px;\r\nline-height:1.2rem;\r\n}\r\nh1,\r\n.bx-def-font-h1 {\r\nline-height:36px;\r\nline-height:2.4rem;\r\n}\r\nh2,\r\n.bx-def-font-h2 {\r\nline-height:29px;\r\nline-height:1.92rem;\r\n}\r\nh3,\r\n.bx-def-font-h3 {\r\nline-height:22px;\r\nline-height:1.44rem;\r\n}\r\n'),
+('bx_protean_styles_custom', @iMixId, 'div.bx-base-pofile-cover {\r\nborder: 1px solid rgba(136, 153, 170, 0.3);\r\nborder-bottom-width: 0;\r\n}\r\n\r\n\r\ndiv.bx-market-unit-cover div.bx-base-text-unit-no-thumb {\r\nborder-width: 0px;\r\n}\r\n\r\n.sys-cpf-header,\r\n.sys-cpf-title,\r\n.sys-cpf-submenu-cnt ul li {\r\nborder-bottom-color: rgba(136, 153, 170, 0.3);\r\n}\r\n\r\n.sys-cpf-submenu-cnt ul li.bx-menu-inter-act {\r\nborder-bottom-color: rgb(17, 34, 51);\r\n}\r\n\r\n\r\n.bx-def-font {\r\nline-height:18px;\r\nline-height:1.2rem;\r\n}\r\nh1,\r\n.bx-def-font-h1 {\r\nline-height:36px;\r\nline-height:2.4rem;\r\n}\r\nh2,\r\n.bx-def-font-h2 {\r\nline-height:29px;\r\nline-height:1.92rem;\r\n}\r\nh3,\r\n.bx-def-font-h3 {\r\nline-height:22px;\r\nline-height:1.44rem;\r\n}\r\n\r\n.bx-def-label {\r\nbackground-color: rgba(245, 250, 255, 0.5);\r\n}\r\n.bx-def-label,\r\n.bx-def-label a,\r\n.bx-def-label .sys-icon {\r\ncolor: rgba(51, 68, 85, 1);\r\n}\r\n\r\n/*--- Forms ---*/\r\n.bx-form-value .bx-form-input-autotoken b {\r\ncolor: rgba(243, 244, 245, 1);\r\n}\r\n\r\n/*--- Timeline ---*/\r\ndiv.bx-tl-view-timeline div.bx-tl-items:before,\r\ndiv.bx-tl-view-timeline div.bx-tl-item-circle {\r\nbackground-color: rgba(51, 68, 85, 1);\r\n}\r\n'),
 ('bx_protean_vpm_font_size_scale', @iMixId, '85%'),
 ('bx_protean_vpt_font_size_scale', @iMixId, '93.75%');
 
