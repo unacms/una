fcd5d8e5475ff372d4211655ce4681d84022febd
AntonLV <vlnotna@gmail.com>
2019-06-25 16:31:05 +0300

    Modules release.

--- modules/boonex/plyr/install/config.php
+++ modules/boonex/plyr/install/config.php
@@ -17,12 +17,12 @@ $aConfig = array(
     'name' => 'bx_plyr',
     'title' => 'Plyr',
     'note' => 'Plyr player integration.',
-    'version' => '9.0.0.DEV',
+    'version' => '10.0.1.DEV',
     'vendor' => 'BoonEx',
-	'help_url' => 'http://feed.una.io/?section={module_name}',
+    'help_url' => 'http://feed.una.io/?section={module_name}',
 
     'compatible_with' => array(
-        '9.0.x'
+        '10.0.x'
     ),
 
     /**
