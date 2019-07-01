7ac755206bea7ade195346bf780d6761ac18749d
AntonLV <vlnotna@gmail.com>
2019-07-01 08:46:58 +0300

    Modules updates.

--- modules/boonex/albums/updates/10.0.0_10.0.1/source/install/config.php
+++ modules/boonex/albums/updates/10.0.0_10.0.1/source/install/config.php
@@ -18,12 +18,12 @@ $aConfig = array(
     'name' => 'bx_albums',
     'title' => 'Albums',
     'note' => 'Photos & videos',
-    'version' => '10.0.2.DEV',
+    'version' => '10.0.1',
     'vendor' => 'BoonEx',
     'help_url' => 'http://feed.una.io/?section={module_name}',
 
     'compatible_with' => array(
-        '10.0.x'
+        '10.0.0-B1'
     ),
 
     /**
