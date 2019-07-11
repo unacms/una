d687b08dab1903c218dcbcfa410a2054e1a96efb
AntonLV <vlnotna@gmail.com>
2019-07-11 11:16:05 +0300

    Modules updates: Back to DEV.

--- modules/boonex/english/install/config.php
+++ modules/boonex/english/install/config.php
@@ -17,7 +17,7 @@ $aConfig = array(
     'name' => 'bx_en',
     'title' => 'English',
     'note' => 'Language file',
-    'version' => '10.0.1.DEV',
+    'version' => '10.0.2.DEV',
     'vendor' => 'Boonex',
     'help_url' => 'http://feed.una.io/?section={module_name}',
 
