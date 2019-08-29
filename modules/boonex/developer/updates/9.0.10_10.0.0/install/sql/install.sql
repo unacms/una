5b676ce4e019b701112ed5aead9c2b915379c281
AntonLV <vlnotna@gmail.com>
2019-08-29 11:39:57 +0300

    Modules updates.

--- modules/boonex/developer/updates/9.0.10_10.0.0/install/sql/install.sql
+++ modules/boonex/developer/updates/9.0.10_10.0.0/install/sql/install.sql
@@ -1,3 +1,6 @@
+SET @sName = 'bx_developer';
+
+
 -- FORMS
 DELETE FROM `sys_form_inputs` WHERE `object`='bx_developer_forms_prevalue' AND `name`='Data';
 INSERT INTO `sys_form_inputs` (`object`, `module`, `name`, `value`, `values`, `checked`, `type`, `caption_system`, `caption`, `info`, `required`, `collapsed`, `html`, `attrs`, `attrs_tr`, `attrs_wrapper`, `checker_func`, `checker_params`, `checker_error`, `db_pass`, `db_params`, `editable`, `deletable`) VALUES
