d9a6bb7745192f8e0ec2fd238f8fb94c3da43fcd
AntonLV <vlnotna@gmail.com>
2019-09-11 15:06:57 +0300

    Modules updates.

--- modules/boonex/lucid/updates/10.0.4_10.0.5/source/install/config.php
+++ modules/boonex/lucid/updates/10.0.4_10.0.5/source/install/config.php
@@ -17,12 +17,12 @@ $aConfig = array(
     'name' => 'bx_lucid',
     'title' => 'Lucid',
     'note' => 'Design template',
-    'version' => '10.0.6.DEV',
+    'version' => '10.0.5',
     'vendor' => 'Boonex',
     'help_url' => 'http://feed.una.io/?section={module_name}',
 
     'compatible_with' => array(
-        '10.0.x'
+        '10.1.0-B1'
     ),
 
     /**
