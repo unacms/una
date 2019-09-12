2c2af5d14a82cbef4823f886fbb18f9ddbb39eed
AntonLV <vlnotna@gmail.com>
2019-09-12 13:33:24 +0300

    Ticket #2368

--- modules/base/groups/classes/BxBaseModGroupsPrivacyPost.php
+++ modules/base/groups/classes/BxBaseModGroupsPrivacyPost.php
@@ -18,7 +18,7 @@ class BxBaseModGroupsPrivacyPost extends BxBaseModProfilePrivacyPost
     {
         parent::__construct($aOptions, $oTemplate);
 
-        $this->_aGroupsExclude = array(BX_DOL_PG_FRIENDS);
+        $this->_aGroupsExclude = array(BX_DOL_PG_FRIENDS, BX_DOL_PG_RELATIONS);
     }
 
     /**
