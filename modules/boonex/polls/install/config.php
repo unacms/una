c35cd0dfc75e283a477d7af8899187242faeac57
AntonLV <vlnotna@gmail.com>
2019-09-23 17:05:24 +0300

    Modules updates.

--- modules/boonex/polls/install/config.php
+++ modules/boonex/polls/install/config.php
@@ -18,7 +18,7 @@ $aConfig = array(
     'name' => 'bx_polls',
     'title' => 'Polls',
     'note' => 'Basic poll module.',
-    'version' => '10.0.3.DEV',
+    'version' => '10.0.4.DEV',
     'vendor' => 'BoonEx',
     'help_url' => 'http://feed.una.io/?section={module_name}',
 
