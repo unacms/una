5b8c13b01f0f44473614c0505f209da9b3b49fbf
AntonLV <vlnotna@gmail.com>
2020-03-10 17:38:57 +0300

    Modules updates.

---
+++ modules/boonex/events/updates/11.0.0_11.0.1/install/sql/enable.sql
@@ -0,0 +1,2 @@
+-- PAGES
+UPDATE `sys_objects_page` SET `override_class_name`='BxEventsPageEntriesInContext', `override_class_file`='modules/boonex/events/classes/BxEventsPageEntriesInContext.php' WHERE `object`='bx_events_context';
