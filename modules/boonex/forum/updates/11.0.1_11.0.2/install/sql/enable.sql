4112d531df12aa3ff9569d06cea64d9c577f26a5
AntonLV <vlnotna@gmail.com>
2020-03-24 12:49:47 +0300

    Modules updates (Release).

---
+++ modules/boonex/forum/updates/11.0.1_11.0.2/install/sql/enable.sql
@@ -0,0 +1,5 @@
+SET @sName = 'bx_forum';
+
+
+-- SETTINGS
+UPDATE `sys_options` SET `caption`='_bx_forum_option_auto_activation_for_categories' WHERE `name`='bx_forum_auto_activation_for_categories';
