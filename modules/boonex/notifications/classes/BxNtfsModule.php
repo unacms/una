44982a3ba7f656c77c3a25b10952e7d41a2984fa
AntonLV <vlnotna@gmail.com>
2020-04-03 15:02:59 +0300

    Ticket #2694

--- modules/boonex/notifications/classes/BxNtfsModule.php
+++ modules/boonex/notifications/classes/BxNtfsModule.php
@@ -1,5 +1,4 @@
-<?php use nspace\func;
-defined('BX_DOL') or die('hack attempt');
+<?php defined('BX_DOL') or die('hack attempt');
 /**
  * Copyright (c) UNA, Inc - https://una.io
  * MIT License - https://opensource.org/licenses/MIT
