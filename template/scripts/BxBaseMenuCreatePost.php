c416f45f69ea3d0cbdae4a26683650759da56d8a
AntonLV <vlnotna@gmail.com>
2020-05-25 17:50:00 +0300

    Ticket #2802

--- template/scripts/BxBaseMenuCreatePost.php
+++ template/scripts/BxBaseMenuCreatePost.php
@@ -29,35 +29,38 @@ class BxBaseMenuCreatePost extends BxTemplMenuCustom
     {
     	$aModules = BxDolModuleQuery::getInstance()->getModulesBy(array('type' => 'all_pairs_name_uri', 'active' => 1));
 
+        $aResult = array();
     	$aMenuItems = $this->_oQuery->getMenuItems();
     	foreach($aMenuItems as $iKey => $aMenuItem) {
+            if((int)$aMenuItem['active'] == 0)
+                continue;
+
             $sModule = $aMenuItem['module'];
             if(!isset($aModules[$sModule]))
                 continue;
 
-            $aMenuItems[$iKey]['id'] = $sModule;
-            $aMenuItems[$iKey]['name'] = $sModule;
-            $aMenuItems[$iKey]['onclick'] = "return " . $this->_sJsObject . ".getForm('" . $sModule . "', '" . $aModules[$sModule] . "', this)";
+            if(BxDolRequest::serviceExists($sModule, 'act_as_profile'))
+                continue;
+
+            $aResult[$iKey] = array_merge($aMenuItem, array(
+                'id' => $sModule,
+                'name' => $sModule,
+                'onclick' => "return " . $this->_sJsObject . ".getForm('" . $sModule . "', '" . $aModules[$sModule] . "', this)"
+            ));
     	}
-        $aMenuItems['more-auto'] = array(
-            'module' => 'system', 
-            'id' => 'more-auto', 
-            'name' => 'more-auto',
-            'title' => '_sys_menu_item_title_va_more_auto', 
-            'href' => 'javascript:void(0)', 
-            'icon' => 'ellipsis-v',
-            'active' => 1
-        );
 
-        return $aMenuItems;
-    }
+        if(!empty($aResult) && is_array($aResult))
+            $aResult['more-auto'] = array(
+                'module' => 'system', 
+                'id' => 'more-auto', 
+                'name' => 'more-auto',
+                'title' => '_sys_menu_item_title_va_more_auto', 
+                'href' => 'javascript:void(0)', 
+                'icon' => 'ellipsis-v',
+                'active' => 1
+            );
 
-    protected function _isVisible ($a)
-    {
-    	if(BxDolRequest::serviceExists($a['module'], 'act_as_profile'))
-            return false;
-
-        return parent::_isVisible($a);
+        return $aResult;
     }
 }
 
