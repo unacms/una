f55c9571078c9410ecb999e9b71fcfd321921a8e
Alex Trofimov <alex.trofimov@boonex.com>
2020-07-27 14:49:00 +1000

    #2874 Nexus: separate setting for list of pages which are accessible for not logged in users

--- modules/boonex/nexus/install/sql/enable.sql
+++ modules/boonex/nexus/install/sql/enable.sql
@@ -11,7 +11,8 @@ VALUES (@iTypeId, 'bx_nexus_general', '_bx_nexus_adm_stg_cpt_category_general', 
 SET @iCategId = LAST_INSERT_ID();
 
 INSERT INTO `sys_options` (`name`, `value`, `category_id`, `caption`, `type`, `extra`, `check`, `check_error`, `order`) VALUES
-('bx_nexus_option_styles', '', @iCategId, '_bx_nexus_option_styles', 'text', '', '', '', 10);
+('bx_nexus_option_styles', '', @iCategId, '_bx_nexus_option_styles', 'text', '', '', '', 10),
+('bx_nexus_option_guest_pages', 'forgot-password,create-account,terms,privacy,contact,about,home', @iCategId, '_bx_nexus_option_guest_pages', 'text', '', '', '', 20);
 
 -- Injections
 
