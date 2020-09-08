b14c8982fb38544082fca54fc41a0b6dd8c415fe
Alexander <rednaxal@gmail.com>
2020-09-08 09:23:41 +0300

    #2842 Reviews: "Featured" menu item appearance fix.

--- modules/boonex/reviews/classes/BxReviewsMenuViewActions.php
+++ modules/boonex/reviews/classes/BxReviewsMenuViewActions.php
@@ -30,6 +30,19 @@ class BxReviewsMenuViewActions extends BxBaseModTextMenuViewActions
     {
         return $this->_getMenuItemByNameActions($aItem);
     }
+
+    protected function _isContentPublic($iContentId) {
+        $CNF = &$this->_oModule->_oConfig->CNF;
+
+        if(empty($CNF['FIELD_ALLOW_VIEW_TO']))
+            return true;
+
+        $aContentInfo = $iContentId == $this->_iContentId ? $this->_aContentInfo : $this->_oModule->_oDb->getContentInfoById($iContentId);
+        if(!isset($aContentInfo[$CNF['FIELD_ALLOW_VIEW_TO']]))
+            return true;
+
+        return $aContentInfo[$CNF['FIELD_ALLOW_VIEW_TO']] < 0 || in_array($aContentInfo[$CNF['FIELD_ALLOW_VIEW_TO']], array(BX_DOL_PG_ALL, BX_DOL_PG_MEMBERS));
+    }
 }
 
 /** @} */
