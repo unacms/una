d37a6b7c341742f73aaafd2d3f66c29776295c1c
RomanL <roman.lesnikov@gmail.com>
2020-10-16 17:02:59 +0300

    #2990 - modules

--- modules/boonex/timeline/classes/BxTimelineFormPost.php
+++ modules/boonex/timeline/classes/BxTimelineFormPost.php
@@ -164,7 +164,7 @@ class BxTimelineFormPost extends BxBaseModGeneralFormEntry
                 'inputs' => array(),
             );
 
-            $oFormNested = new BxDolFormNested('photo', $aFormNested, 'tlb_do_submit', $this->_oModule->_oTemplate);
+            $oFormNested = new BxDolFormNestedGhost('photo', $aFormNested, 'tlb_do_submit', $this->_oModule->_oTemplate);
 
             $this->aInputs['photo']['storage_object'] = $this->_oModule->_oConfig->getObject('storage_photos');
             $this->aInputs['photo']['images_transcoder'] = $this->_oModule->_oConfig->getObject('transcoder_photos_preview');
