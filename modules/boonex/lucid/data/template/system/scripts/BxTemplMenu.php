ed9111031bb5c65974e1b1033c32737ea89d1555
AntonLV <vlnotna@gmail.com>
2020-11-11 14:38:30 +0300

    Ticket #2496

--- modules/boonex/lucid/data/template/system/scripts/BxTemplMenu.php
+++ modules/boonex/lucid/data/template/system/scripts/BxTemplMenu.php
@@ -15,6 +15,20 @@ class BxTemplMenu extends BxBaseMenu
     public function __construct ($aObject, $oTemplate = false)
     {
         parent::__construct ($aObject, $oTemplate);
+        
+        $this->_aOptionalParams['popup'] = '';
+    }
+
+    protected function _getTemplateVars()
+    {
+        return array_merge(parent::_getTemplateVars(), array(
+            'html_id' => '',
+            'bx_if:show_more_auto_class' => array(
+                'condition' => false,
+                'content' => array()
+            ),
+            'js_code' => ''
+        ));
     }
 }
 
