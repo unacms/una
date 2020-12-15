3247a344cdc74bb57751d73257f7891d746f5041
AntonLV <vlnotna@gmail.com>
2020-12-15 08:51:55 +0300

    Ticket #3086

--- modules/boonex/classes/install/sql/install.sql
+++ modules/boonex/classes/install/sql/install.sql
@@ -203,6 +203,7 @@ CREATE TABLE IF NOT EXISTS `bx_classes_cmts` (
   `cmt_rate_count` int(11) NOT NULL DEFAULT '0',
   `cmt_time` int(11) unsigned NOT NULL DEFAULT '0',
   `cmt_replies` int(11) NOT NULL DEFAULT '0',
+  `cmt_pinned` int(11) NOT NULL default '0',
   PRIMARY KEY (`cmt_id`),
   KEY `cmt_object_id` (`cmt_object_id`,`cmt_parent_id`),
   FULLTEXT KEY `search_fields` (`cmt_text`)
@@ -221,6 +222,7 @@ CREATE TABLE IF NOT EXISTS `bx_classes_cmts_notes` (
   `cmt_rate_count` int(11) NOT NULL DEFAULT '0',
   `cmt_time` int(11) unsigned NOT NULL DEFAULT '0',
   `cmt_replies` int(11) NOT NULL DEFAULT '0',
+  `cmt_pinned` int(11) NOT NULL default '0',
   PRIMARY KEY (`cmt_id`),
   KEY `cmt_object_id` (`cmt_object_id`,`cmt_parent_id`),
   FULLTEXT KEY `search_fields` (`cmt_text`)
