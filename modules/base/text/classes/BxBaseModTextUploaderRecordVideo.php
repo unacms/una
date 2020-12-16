f4abdb12a80d7e4b62cc9596f7156cf3c0dc17fb
Alexander <rednaxal@gmail.com>
2020-12-16 15:35:53 +0300

    #3081 Record Video Uploader: Integrated to base/text attachments kind of uploaders

---
+++ modules/base/text/classes/BxBaseModTextUploaderRecordVideo.php
@@ -0,0 +1,25 @@
+<?php defined('BX_DOL') or die('hack attempt');
+/**
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
+ *
+ * @defgroup    BaseText Base classes for text modules
+ * @ingroup     UnaModules
+ *
+ * @{
+ */
+
+class BxBaseModTextUploaderRecordVideo extends BxTemplUploaderRecordVideo
+{
+    protected $_sModule;
+    protected $_oModule;
+
+    public function __construct ($aObject, $sStorageObject, $sUniqId, $oTemplate)
+    {
+        $this->_oModule = BxDolModule::getInstance($this->_sModule);
+
+        parent::__construct($aObject, $sStorageObject, $sUniqId, $this->_oModule->_oTemplate);
+    }
+}
+
+/** @} */
