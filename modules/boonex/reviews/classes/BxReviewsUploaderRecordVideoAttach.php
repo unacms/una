bd72e9402bb317ec6335bdca0feec9e6299a71c9
Alexander <rednaxal@gmail.com>
2020-12-16 16:12:49 +0300

    #3081 Record Video Uploader: Integrated to Reviews module

---
+++ modules/boonex/reviews/classes/BxReviewsUploaderRecordVideoAttach.php
@@ -0,0 +1,27 @@
+<?php defined('BX_DOL') or die('hack attempt');
+/**
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
+ *
+ * @defgroup    Reviews Reviews
+ * @ingroup     UnaModules
+ *
+ * @{
+ */
+
+class BxReviewsUploaderRecordVideoAttach extends BxBaseModTextUploaderRecordVideoAttach
+{
+    public function __construct ($aObject, $sStorageObject, $sUniqId, $oTemplate)
+    {
+        $this->_sModule = 'bx_reviews';
+
+        parent::__construct($aObject, $sStorageObject, $sUniqId, $oTemplate);
+    }
+
+    protected function isAdmin ($iContentId = 0)
+    {
+        return $this->_oModule->_isModerator (false);
+    }
+}
+
+/** @} */
