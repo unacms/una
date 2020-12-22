02ec60db4d2f1b2dc6c036518c9fab833b1c02b3
AntonLV <vlnotna@gmail.com>
2020-12-22 16:34:33 +0300

    Modules updates: Back to DEV.

--- modules/boonex/persons/install/config.php
+++ modules/boonex/persons/install/config.php
@@ -18,7 +18,7 @@ $aConfig = array(
     'name' => 'bx_persons',
     'title' => 'Persons',
     'note' => 'Basic person profiles functionality.',
-    'version' => '11.0.2.DEV',
+    'version' => '11.0.3.DEV',
     'vendor' => 'BoonEx',
     'help_url' => 'http://feed.una.io/?section={module_name}',
 
