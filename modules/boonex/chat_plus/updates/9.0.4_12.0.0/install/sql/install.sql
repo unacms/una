cf430bb76ffd1f774216ba24ff0e02e15284a033
AntonLV <vlnotna@gmail.com>
2020-12-30 09:19:12 +0300

    Modules updates

--- modules/boonex/chat_plus/updates/9.0.4_12.0.0/install/sql/install.sql
+++ modules/boonex/chat_plus/updates/9.0.4_12.0.0/install/sql/install.sql
@@ -1,3 +1,3 @@
 -- STUDIO PAGE & WIDGET
 SET @iPageId = (SELECT `id` FROM `sys_std_pages` WHERE `name`='bx_chat_plus' LIMIT 1);
-UPDATE `sys_std_widgets` SET `type`='content' WHERE `page_id`=@iPageId;
+UPDATE `sys_std_widgets` SET `type`='integrations' WHERE `page_id`=@iPageId;
