62d9ad03534e5df4f9d269ca01cde015a9132b38
Alex Trofimov <alex.trofimov@boonex.com>
2020-12-30 19:21:43 +1100

    #2820 Events: timezone setting is ignored for date interpretation.

--- modules/boonex/events/updates/11.0.4_12.0.0/install/installer.php
+++ modules/boonex/events/updates/11.0.4_12.0.0/install/installer.php
@@ -24,6 +24,29 @@ class BxEventsUpdater extends BxDolStudioUpdater
 
             if(!$this->oDb->isFieldExists('bx_events_favorites_track', 'added'))
                 $this->oDb->query("ALTER TABLE `bx_events_favorites_track` ADD `list_id` int(11) NOT NULL default '0' AFTER `author_id`");
+
+            $aEvents = $this->oDb->getAll('SELECT `id`, `date_start`, `date_end`, `timezone` FROM `bx_events_data` ORDER BY `id`');
+            if ($aEvents) {
+                foreach ($aEvents as $aEvent) {
+                   if (!$aEvent['timezone'] || 'UTC' == $aEvent['timezone'])
+                        continue;
+
+                    $oDateStart = $aEvent['date_start'] ? date_create('@' . $aEvent['date_start']) : 0;
+                    $oDateEnd = $aEvent['date_end'] ? date_create('@' . $aEvent['date_end']) : 0;
+                    if (!$oDateStart && !$oDateEnd)
+                        continue;
+
+                    $oTz = timezone_open($aEvent['timezone']);
+                    if (!$oTz)
+                        continue;
+                    $iOffset = timezone_offset_get($oTz, $oDateStart ? $oDateStart : $oDateEnd);
+
+                    if ($aEvent['date_start'])
+                        $this->oDb->query("UPDATE `bx_events_data` SET `date_start` = :date WHERE `id` = :id", array('date' =>  $aEvent['date_start'] - $iOffset, 'id' => $aEvent['id']));
+                    if ($aEvent['date_end'])
+                        $this->oDb->query("UPDATE `bx_events_data` SET `date_end` = :date WHERE `id` = :id", array('date' =>  $aEvent['date_end'] - $iOffset, 'id' => $aEvent['id']));
+                }
+            }
         }
 
         return parent::actionExecuteSql($sOperation);
