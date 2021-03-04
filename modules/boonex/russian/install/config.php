ea22f86cd842f3e7988a0189d6a2a14022545cdd
AntonLV <vlnotna@gmail.com>
2021-03-04 16:36:13 +0300

    Modules updates: Back to DEV.

--- modules/boonex/russian/install/config.php
+++ modules/boonex/russian/install/config.php
@@ -17,7 +17,7 @@ $aConfig = array(
     'name' => 'bx_ru',
     'title' => 'Russian',
     'note' => 'Language file',
-    'version' => '12.0.4.DEV',
+    'version' => '12.0.5.DEV',
     'vendor' => 'Boonex',
     'help_url' => 'http://feed.una.io/?section={module_name}',
 
