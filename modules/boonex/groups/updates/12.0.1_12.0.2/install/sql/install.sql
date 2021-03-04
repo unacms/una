7e1558706a43a1a450e25d53931e070bd901fcc6
AntonLV <vlnotna@gmail.com>
2021-03-04 09:00:08 +0300

    Modules updates.

---
+++ modules/boonex/groups/updates/12.0.1_12.0.2/install/sql/install.sql
@@ -0,0 +1,2 @@
+-- COMMENTS
+UPDATE `sys_objects_cmts` SET `ClassName`='BxTemplCmtsNotes' WHERE `Name`='bx_groups_notes';
---
+++ modules/boonex/market/updates/12.0.1_12.0.2/install/sql/install.sql
@@ -0,0 +1,2 @@
+-- COMMENTS
+UPDATE `sys_objects_cmts` SET `ClassName`='BxTemplCmtsNotes' WHERE `Name`='bx_market_notes';
