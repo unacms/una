14e9e2db4aa1c3540aea9c55e602ebcd4fd93d1b
Alexander <rednaxal@gmail.com>
2021-03-24 16:49:29 +0300

    #3068 Files: Bulk downloading reworked. Added support for delayed downloads in case of remote storage is used.

--- modules/boonex/files/classes/BxFilesCronProcessData.php
+++ modules/boonex/files/classes/BxFilesCronProcessData.php
@@ -14,6 +14,7 @@ class BxFilesCronProcessData extends BxDolCron
     function processing()
     {
         BxDolService::call('bx_files', 'process_files_data', array(3));
+        BxDolService::call('bx_files', 'prune_downloading_jobs', array());
     }
 }
 
