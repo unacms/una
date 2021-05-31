8b23182a24ff7650ac58c546587dd3fe9f749a51
Alexander <rednaxal@gmail.com>
2021-05-31 10:03:24 +0300

    Revert "#3399 Help Tours module initial commit."
    This reverts commit 9a435ee3
    
    (wrong ticket number)

--- modules/boonex/help_tours/install/sql/disable.sql
+++
@@ -1,4 +0,0 @@
--- GRIDS
-DELETE FROM `sys_objects_grid` WHERE `object` LIKE 'bx_help_tours_%';
-DELETE FROM `sys_grid_fields` WHERE `object` LIKE 'bx_help_tours_%';
-DELETE FROM `sys_grid_actions` WHERE `object` LIKE 'bx_help_tours_%';
