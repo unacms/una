ec36151778c8c661a81e848d8576b8128c25bba4
AntonLV <vlnotna@gmail.com>
2021-06-25 13:22:53 +0300

    Modules updates.

---
+++ modules/boonex/english/updates/12.0.2_12.0.3/install/config.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
+ */
+
+$aConfig = array(
+    /**
+     * Main Section.
+     */
+    'title' => 'English',
+    'version_from' => '12.0.2',
+    'version_to' => '12.0.3',
+    'vendor' => 'BoonEx',
+
+    'compatible_with' => array(
+        '12.0.x',
+        '12.1.x'
+    ),
+
+    /**
+     * 'home_dir' and 'home_uri' - should be unique. Don't use spaces in 'home_uri' and the other special chars.
+     */
+    'home_dir' => 'boonex/english/updates/update_12.0.2_12.0.3/',
+    'home_uri' => 'en_update_1202_1203',
+
+    'module_dir' => 'boonex/english/',
+    'module_uri' => 'en',
+
+    'db_prefix' => 'bx_eng_',
+    'class_prefix' => 'BxEng',
+
+    /**
+     * Installation/Uninstallation Section.
+     */
+    'install' => array(
+        'execute_sql' => 0,
+        'update_files' => 1,
+        'update_languages' => 1,
+        'restore_languages' => 0,
+        'clear_db_cache' => 0,
+    ),
+
+    /**
+     * Category for language keys.
+     */
+    'language_category' => 'System',
+
+    /**
+     * Files Section
+     */
+    'delete_files' => array(),
+);
