18d5ea71815f016c62997387d547cfc9abb273ea
AntonLV <vlnotna@gmail.com>
2021-06-28 11:40:14 +0300

    Ticket #3431

--- template/scripts/BxBaseMenuProfileStats.php
+++ template/scripts/BxBaseMenuProfileStats.php
@@ -11,13 +11,42 @@
  * Menu representation.
  * @see BxDolMenu
  */
-class BxBaseMenuProfileStats extends BxTemplMenu
+class BxBaseMenuProfileStats extends BxTemplMenuAccountNotifications
 {
     public function __construct ($aObject, $oTemplate)
     {
         parent::__construct ($aObject, $oTemplate);
 
         $this->_bDisplayAddons = true;
+    }
+
+    protected function getMenuItemsRaw ()
+    {
+        $aItems = $this->_oQuery->getMenuItemsBy(array(
+            'type' => 'set_name', 
+            'set_name' => $this->_aObject['set_name']
+        ));
+
+        $aDuplicates = $this->_oQuery->getMenuItemsBy(array(
+            'type' => 'set_name_duplicates', 
+            'set_name' => $this->_aObject['set_name']
+        ));
+
+        $oProfile = BxDolProfile::getInstance();
+        if(!$oProfile)
+            return array();
+
+        $sModule = $oProfile->getModule();
+
+        $aResult = array();
+        foreach($aItems as $aItem) {
+            if(in_array($aItem['name'], $aDuplicates) && $aItem['module'] != $sModule)
+                continue;
+            
+            $aResult[$aItem['name']] = $aItem;
+        }
+
+        return $aResult;
     }
 }
 
