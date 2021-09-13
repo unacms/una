c8f93d0f17325f07346958f4905c80004a3f532b
AntonLV <vlnotna@gmail.com>
2021-09-13 17:03:50 +0300

    Ticket #3399 - Menus, Splash, etc.

--- modules/boonex/artificer/js/tailwind.config.js
+++ modules/boonex/artificer/js/tailwind.config.js
@@ -2,12 +2,33 @@ module.exports = {
   purge: [],
   darkMode: 'media', // false or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        animation: {
+          goo: "goo 8s infinite",
+        },
+        keyframes: {
+          goo: {
+            "0%": {
+              transform: "translate(0px, 0px) scale(1)",
+            },
+            "33%": {
+              transform: "translate(30px, -50px) scale(1.2)",
+            },
+            "66%": {
+              transform: "translate(-20px, 20px) scale(0.8)",
+            },
+            "100%": {
+              transform: "translate(0px, 0px) scale(1)",
+            },
+          },
+        },
+    },
   },
   variants: {
     extend: {
         margin: ['first', 'last'],
         padding: ['first', 'last'],
+        ringColor: ['hover', 'active'],
     },
   },
   plugins: [],
