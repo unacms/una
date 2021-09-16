9d86582557cfdc9866d49bbe15101b08718c49d8
AntonLV <vlnotna@gmail.com>
2021-09-16 12:10:06 +0300

    Ticket #3399 - Popup Menus, Block Tabs, Timeline card menus.

--- modules/boonex/artificer/classes/BxArtificerModule.php
+++ modules/boonex/artificer/classes/BxArtificerModule.php
@@ -25,6 +25,30 @@ class BxArtificerModule extends BxBaseModTemplateModule
 
         return $this->_oTemplate->getIncludeCssJs();
     }
+
+    public function serviceGetBlockSplash()
+    {
+        if($this->_oTemplate->getCode() != $this->_oConfig->getUri())
+            return '';
+
+        $oPermalink = BxDolPermalinks::getInstance();
+
+        $sJoinForm = $sLoginForm = '';
+        if(!isLogged()) {
+            $sJoinForm = BxDolService::call('system', 'create_account_form', array(), 'TemplServiceAccount');
+            $sLoginForm = BxDolService::call('system', 'login_form', array(), 'TemplServiceLogin');
+        }
+
+        $this->_oTemplate->addJs(array('lottie.min.js'));
+        return $this->_oTemplate->parseHtmlByName('splash.html', array(
+            'join_link' => BX_DOL_URL_ROOT . $oPermalink->permalink('page.php?i=create-account'),
+            'join_form' => $sJoinForm,
+            'join_form_in_box' => !empty($sJoinForm) ? DesignBoxContent(_t('_sys_txt_splash_join'), $sJoinForm, BX_DB_PADDING_DEF) : '',
+            'login_link' => BX_DOL_URL_ROOT . $oPermalink->permalink('page.php?i=login'),
+            'login_form' => $sLoginForm,
+            'login_form_in_box' => !empty($sLoginForm) ? DesignBoxContent(_t('_sys_txt_splash_login'), $sLoginForm, BX_DB_PADDING_DEF) : ''
+        ));
+    }
 }
 
 /** @} */
