440329a3efb8468d35ed45342013dec4747593b3
RomanL <roman.lesnikov@gmail.com>
2021-10-11 16:16:14 +0300

    #3399 - Artificer small changes

--- modules/boonex/artificer/data/template/system/scripts/BxTemplCmtsMenuUnitMeta.php
+++ modules/boonex/artificer/data/template/system/scripts/BxTemplCmtsMenuUnitMeta.php
@@ -16,6 +16,25 @@ class BxTemplCmtsMenuUnitMeta extends BxBaseCmtsMenuUnitMeta
     {
         parent::__construct ($aObject, $oTemplate);
     }
+    
+    protected function _getMenuItemAuthor($aItem)
+    {
+        list($sAuthorName, $sAuthorLink, $sAuthorIcon, $sAuthorUnit, $sAuthorBadges) = $this->_oCmts->getAuthorInfo($this->_aCmt['cmt_author_id']);
+    
+        $sResult = '';
+        if(!empty($sAuthorLink))
+            $sResult = $this->getUnitMetaItemLink($sAuthorName, array(
+                'href' => $sAuthorLink,
+                'class' => $this->_sStylePrefix . '-username whitespace-nowrap hover:underline text-gray-700 dark:text-gray-200 hover:text-gray-900 dark:hover:text-white font-semibold',
+                'title' => bx_html_attribute($sAuthorName),
+            )). $sAuthorBadges;
+        else
+            $sResult = $this->getUnitMetaItemText($sAuthorName, array(
+                'class' => $this->_sStylePrefix . '-username whitespace-nowrap text-gray-700 dark:text-gray-200 font-semibold'
+            )). $sAuthorBadges;
+
+        return $sResult;
+    }
 }
 
 /** @} */
