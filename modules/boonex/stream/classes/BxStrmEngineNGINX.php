2ad64d0cb508e58d353baa7ff7fcfef17a673440
Alex Trofimov <alex.trofimov@boonex.com>
2021-12-10 19:27:05 +1100

    #3608 Stream: authentication for Nginx streaming

--- modules/boonex/stream/classes/BxStrmEngineNGINX.php
+++ modules/boonex/stream/classes/BxStrmEngineNGINX.php
@@ -54,8 +54,10 @@ class BxStrmEngineNGINX extends BxDol
             return false;
 
         $sBaseUrl = "rtmp://$sHost:1935/$sApp";
-        $sStreamSecret = base_convert(substr(md5(BX_DOL_SECRET . $sStreamKey), -4), 16, 36);
-        return ['server' => $sBaseUrl, 'key' => $sStreamKey . '?s=' . $sStreamSecret];
+        $sParams = '';
+        if (getParam('bx_stream_server_nginx_auth'))
+            $sParams = '?s=' . base_convert(substr(md5(BX_DOL_SECRET . $sStreamKey), -4), 16, 36);
+        return ['server' => $sBaseUrl, 'key' => $sStreamKey . $sParams];
     }
 
     public function getRtmpIngestUrl($sStreamKey)
