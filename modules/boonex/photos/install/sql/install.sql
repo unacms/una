27a09c0d84aed9f2d27d701af188959c6e1ac70b
RomanL <roman.lesnikov@gmail.com>
2022-04-01 15:53:02 +0600

    #3262 - Make sure that primary index is in every table

--- modules/boonex/photos/install/sql/install.sql
+++ modules/boonex/photos/install/sql/install.sql
@@ -108,9 +108,11 @@ CREATE TABLE IF NOT EXISTS `bx_photos_cmts_notes` (
 
 -- TABLE: votes
 CREATE TABLE IF NOT EXISTS `bx_photos_votes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `object_id` int(11) NOT NULL default '0',
   `count` int(11) NOT NULL default '0',
   `sum` int(11) NOT NULL default '0',
+  PRIMARY KEY (`id`),
   UNIQUE KEY `object_id` (`object_id`)
 );
 
@@ -126,9 +128,11 @@ CREATE TABLE IF NOT EXISTS `bx_photos_votes_track` (
 );
 
 CREATE TABLE IF NOT EXISTS `bx_photos_svotes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `object_id` int(11) NOT NULL default '0',
   `count` int(11) NOT NULL default '0',
   `sum` int(11) NOT NULL default '0',
+  PRIMARY KEY (`id`),
   UNIQUE KEY `object_id` (`object_id`)
 );
 
@@ -144,10 +148,12 @@ CREATE TABLE IF NOT EXISTS `bx_photos_svotes_track` (
 );
 
 CREATE TABLE IF NOT EXISTS `bx_photos_reactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `object_id` int(11) NOT NULL default '0',
   `reaction` varchar(32) NOT NULL default '',
   `count` int(11) NOT NULL default '0',
   `sum` int(11) NOT NULL default '0',
+  PRIMARY KEY (`id`),
   UNIQUE KEY `reaction` (`object_id`, `reaction`)
 );
 
@@ -165,24 +171,30 @@ CREATE TABLE IF NOT EXISTS `bx_photos_reactions_track` (
 
 -- TABLE: views
 CREATE TABLE `bx_photos_views_track` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `object_id` int(11) NOT NULL default '0',
   `viewer_id` int(11) NOT NULL default '0',
   `viewer_nip` int(11) unsigned NOT NULL default '0',
   `date` int(11) NOT NULL default '0',
+  PRIMARY KEY (`id`),
   KEY `id` (`object_id`,`viewer_id`,`viewer_nip`)
 );
 
 -- TABLE: metas
 CREATE TABLE `bx_photos_meta_keywords` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `object_id` int(10) unsigned NOT NULL,
   `keyword` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `object_id` (`object_id`),
   KEY `keyword` (`keyword`)
 );
 
 CREATE TABLE `bx_photos_meta_keywords_camera` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `object_id` int(10) unsigned NOT NULL,
   `keyword` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `object_id` (`object_id`),
   KEY `keyword` (`keyword`)
 );
@@ -202,16 +214,20 @@ CREATE TABLE `bx_photos_meta_locations` (
 );
 
 CREATE TABLE `bx_photos_meta_mentions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `object_id` int(10) unsigned NOT NULL,
   `profile_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `object_id` (`object_id`),
   KEY `profile_id` (`profile_id`)
 );
 
 -- TABLE: reports
 CREATE TABLE IF NOT EXISTS `bx_photos_reports` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `object_id` int(11) NOT NULL default '0',
   `count` int(11) NOT NULL default '0',
+  PRIMARY KEY (`id`),
   UNIQUE KEY `object_id` (`object_id`)
 );
 
@@ -252,9 +268,11 @@ CREATE TABLE `bx_photos_favorites_lists` (
 
 -- TABLE: scores
 CREATE TABLE IF NOT EXISTS `bx_photos_scores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `object_id` int(11) NOT NULL default '0',
   `count_up` int(11) NOT NULL default '0',
   `count_down` int(11) NOT NULL default '0',
+  PRIMARY KEY (`id`),
   UNIQUE KEY `object_id` (`object_id`)
 );
 
