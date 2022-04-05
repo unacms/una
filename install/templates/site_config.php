2765ded88153c16ef6e8799b717ccfb51ccbe6dc
Alex Trofimov <alex.trofimov@boonex.com>
2022-04-05 14:56:27 +1000

    #3734 Install script design is broken

--- install/templates/site_config.php
+++ install/templates/site_config.php
@@ -1,6 +1,6 @@
 <div class="bx-install-site-config">
 
-    <h1><?php echo _t('_sys_inst_site_config'); ?></h1>
+    <h1 class="bx-def-font-h1"><?php echo _t('_sys_inst_site_config'); ?></h1>
 
    <?=$sForm; ?>
 
