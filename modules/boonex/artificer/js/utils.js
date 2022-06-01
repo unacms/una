6dd5ca8946adb4f68356227eaaffeb893a7a2df8
roman.lesnikov@gmail.com <roman.lesnikov@gmail.com>
2022-06-01 11:11:51 +0300

    #3815 - Artificer: Light/Dark moder switcher should work as dropdown popup.

--- modules/boonex/artificer/js/utils.js
+++ modules/boonex/artificer/js/utils.js
@@ -1,33 +1,50 @@
 //for custom JS
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) 
-        $('html').addClass('dark')
-
 $(document).ready(function() {
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        $('.bx-sb-theme-switcher .sys-icon').addClass('sun');
-    } else {
-        document.documentElement.classList.remove('dark');
-        $('.bx-sb-theme-switcher .sys-icon').addClass('moon');
-    }
+	bx_artificer_set_color_scheme_icon();
 });
 
-function bx_artificer_set_color_scheme(){
-    v = 'moon'
-    if($('.bx-sb-theme-switcher .sys-icon').hasClass('moon')){
-        v = 'sun';
-        $('.bx-sb-theme-switcher .sys-icon').addClass('sun').removeClass('moon')
-    }
-    else{
-        $('.bx-sb-theme-switcher .sys-icon').addClass('moon').removeClass('sun')
-    }
-    
-    if (v == 'sun'){
-        $('html').addClass('dark');
-        localStorage.theme = 'dark'
-    }
-    else{
-        $('html').removeClass('dark');
-        localStorage.theme = ''
-    }   
+function bx_artificer_set_color_scheme_html(){
+	if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) 
+        $('html').addClass('dark')
+	else
+		$('html').removeClass('dark')
 }
 
+function bx_artificer_set_color_scheme_icon(){
+	if (localStorage.theme === 'dark') {
+        $('.bx-sb-theme-switcher .sys-icon').addClass('moon').removeClass('sun').removeClass('desktop');
+    } 
+	if (localStorage.theme === 'sun') {
+        $('.bx-sb-theme-switcher .sys-icon').addClass('sun').removeClass('moon').removeClass('desktop');
+    } 
+	if (!('theme' in localStorage))
+		$('.bx-sb-theme-switcher .sys-icon').addClass('desktop').removeClass('moon').removeClass('sun');
+}
+
+function bx_artificer_set_color_scheme(m){
+	switch (m) {
+	  case 0:
+		localStorage.removeItem('theme');
+		break;
+	  case 1:
+		localStorage.theme = 'sun'
+		break;
+	  case 2:
+		localStorage.theme = 'dark'
+		break;
+	}
+	bx_artificer_set_color_scheme_icon();
+	bx_artificer_set_color_scheme_html();
+}
+
+function bx_artificer_get_color_scheme_menu(){
+	$('#bx-sb-theme-switcher-menu').dolPopup({
+		pointer: {
+            el: $('.bx-sb-theme-switcher')
+        }, 
+        moveToDocRoot: true,
+        cssClass: 'bx-popup-menu'
+	});
+}
+
+bx_artificer_set_color_scheme_html();
