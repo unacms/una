8fc6c4a31c2aa20a0dd0eb30c04ba382cc327ea3
AntonLV <vlnotna@gmail.com>
2022-06-07 10:45:04 +0300

    Ticket #3863 - Add popup doesn't work when [+] item was hidden from Member Toolbar

--- modules/boonex/artificer/js/utils.js
+++ modules/boonex/artificer/js/utils.js
@@ -1,50 +1,62 @@
 //for custom JS
 $(document).ready(function() {
-	bx_artificer_set_color_scheme_icon();
+    bx_artificer_set_color_scheme_icon();
 });
 
 function bx_artificer_set_color_scheme_html(){
-	if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) 
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) 
         $('html').addClass('dark')
-	else
-		$('html').removeClass('dark')
+    else
+        $('html').removeClass('dark')
 }
 
 function bx_artificer_set_color_scheme_icon(){
-	if (localStorage.theme === 'dark') {
+    if (localStorage.theme === 'dark') {
         $('.bx-sb-theme-switcher .sys-icon').addClass('moon').removeClass('sun').removeClass('desktop');
-    } 
-	if (localStorage.theme === 'sun') {
+    }
+
+    if (localStorage.theme === 'sun') {
         $('.bx-sb-theme-switcher .sys-icon').addClass('sun').removeClass('moon').removeClass('desktop');
-    } 
-	if (!('theme' in localStorage))
-		$('.bx-sb-theme-switcher .sys-icon').addClass('desktop').removeClass('moon').removeClass('sun');
+    }
+
+    if (!('theme' in localStorage))
+        $('.bx-sb-theme-switcher .sys-icon').addClass('desktop').removeClass('moon').removeClass('sun');
 }
 
 function bx_artificer_set_color_scheme(m){
-	switch (m) {
-	  case 0:
-		localStorage.removeItem('theme');
-		break;
-	  case 1:
-		localStorage.theme = 'sun'
-		break;
-	  case 2:
-		localStorage.theme = 'dark'
-		break;
-	}
-	bx_artificer_set_color_scheme_icon();
-	bx_artificer_set_color_scheme_html();
+    switch (m) {
+      case 0:
+        localStorage.removeItem('theme');
+        break;
+      case 1:
+        localStorage.theme = 'sun'
+        break;
+      case 2:
+        localStorage.theme = 'dark'
+        break;
+    }
+    bx_artificer_set_color_scheme_icon();
+    bx_artificer_set_color_scheme_html();
 }
 
 function bx_artificer_get_color_scheme_menu(){
-	$('#bx-sb-theme-switcher-menu').dolPopup({
-		pointer: {
+    $('#bx-sb-theme-switcher-menu').dolPopup({
+        pointer: {
             el: $('.bx-sb-theme-switcher')
-        }, 
+        },
         moveToDocRoot: true,
         cssClass: 'bx-popup-menu'
-	});
+    });
+}
+
+function bx_artificer_add_content_panel(sMenu, e, sPosition) {
+    $('.bx-popup-applied:visible').dolPopupHide();
+
+    var sSidebar = 'account';
+    if(bx_sidebar_active(sSidebar))
+        bx_sidebar_toggle(sSidebar);
+
+    bx_menu_popup_inline(sMenu, e, {});
 }
 
 bx_artificer_set_color_scheme_html();
