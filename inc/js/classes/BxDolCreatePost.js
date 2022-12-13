1cdac7a39104ec42af2a445b43d41abeb5ea3d77
AntonLV <vlnotna@gmail.com>
2022-12-13 14:20:00 +0300

    Ticket #4152 - United Post Form: Update forms loading/switching effects.

--- inc/js/classes/BxDolCreatePost.js
+++ inc/js/classes/BxDolCreatePost.js
@@ -55,7 +55,6 @@ BxDolCreatePost.prototype.init = function () {
 
 BxDolCreatePost.prototype.getForm = function (sModuleName, sModuleUri, oElement) {
     var $this = this;
-    var oDate = new Date();
 
     var oTab = $(oElement);
     var sTabActive = 'bx-menu-tab-active';
@@ -63,47 +62,44 @@ BxDolCreatePost.prototype.getForm = function (sModuleName, sModuleUri, oElement)
     oTab.parents('li:first').addClass(sTabActive);
     oTab.parents('.bx-popup-applied:visible').dolPopupHide();
 
-    if($('.sys-cpf-form.sys-cpf-' + sModuleName + ' form').length > 0) {
-        $('.sys-cpf-form:visible').bx_anim('hide', this._sAnimationEffect, this._iAnimationSpeed, function() {
-            $('.sys-cpf-form.sys-cpf-' + sModuleName).show().siblings('.sys-cpf-close:hidden').show();
-        });
+    $('.sys-cpf-form:visible').hide({
+        duration: 0,
+        complete: function() {
+            $('.sys-cpf-close:visible').hide();
 
-        return false;
-    }
+            if(!$('.sys-cpf-form.sys-cpf-' + sModuleName + ' form').length) {
+                var oDate = new Date();
 
-    this._loadingInBlock(oElement, true);
+                $this._placeholderInBlock(oElement, true);
 
-    $.get(
-        this._sRootUrl + 'modules/?r=' + sModuleUri + '/get_create_post_form/', {
-            ajax_mode: true,
-            dynamic_mode: true,
-            absolute_action_url: true,
-            context_id: this._iContextId,
-            custom: this._oCustom,
-            _t:oDate.getTime()
-        },
-        function(oData) {
-            $this._loadingInBlock(oElement, false);
+                $.get(
+                    $this._sRootUrl + 'modules/?r=' + sModuleUri + '/get_create_post_form/', {
+                        ajax_mode: true,
+                        dynamic_mode: true,
+                        absolute_action_url: true,
+                        context_id: $this._iContextId,
+                        custom: $this._oCustom,
+                        _t: oDate.getTime()
+                    },
+                    function(oData) {
+                        $this._placeholderInBlock(oElement, false);
 
-            if(!oData || !oData.content) 
-                return;
+                        if(!oData || !oData.content) 
+                            return;
 
-            $('.sys-cpf-form:visible').bx_anim('hide', this._sAnimationEffect, this._iAnimationSpeed, function() {
-                $this._loadingInBlock(oElement, true);
+                        var sForm = '.sys-cpf-form.sys-cpf-' + oData.module;
+                        if($(sForm).length == 0)
+                            $('.sys-cpf-close').before($('<div class="sys-cpf-form sys-cpf-' + oData.module + '"></div>').hide());
 
-                $('.sys-cpf-close:visible').hide();
-
-                var sForm = '.sys-cpf-form.sys-cpf-' + oData.module;
-                if($(sForm).length == 0)
-                    $('.sys-cpf-close').before($('<div class="sys-cpf-form sys-cpf-' + oData.module + '"></div>').hide());
-
-                $(sForm).html(oData.content).show().siblings('.sys-cpf-close:hidden').show();
-
-                $this._loadingInBlock(oElement, false);
-            });
-        }, 
-        'json'
-    );
+                        $(sForm).html(oData.content).show().siblings('.sys-cpf-close:hidden').show();
+                    }, 
+                    'json'
+                );
+            }
+            else 
+                $('.sys-cpf-form.sys-cpf-' + sModuleName).show().siblings('.sys-cpf-close:hidden').show();
+        }
+    });
 
     return false;
 };
@@ -134,8 +130,22 @@ BxDolCreatePost.prototype._loading = function(e, bShow) {
 };
 
 BxDolCreatePost.prototype._loadingInBlock = function(e, bShow) {
-    var oParent = $(e).length ? $(e).parents('.bx-db-content:first') : $('body'); 
+    var oParent = $(e).length ? $(e).parents('.bx-db-container:first') : $('body'); 
     bx_loading(oParent, bShow);
+};
+
+BxDolCreatePost.prototype._placeholderInBlock = function(e, bShow) {
+    var oPlaceholder = null;
+    if($(e).length) 
+        oPlaceholder = $(e).parents('.bx-db-container:first').find('.sys-cpf-form.sys-cpf-loading'); 
+
+    if(!oPlaceholder)
+        return _loadingInBlock(e, bShow);
+
+    if(bShow)
+        oPlaceholder.show();
+    else
+        oPlaceholder.hide();
 };
 
 /** @} */
