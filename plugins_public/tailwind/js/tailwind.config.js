bcc2e1b71319800b69f97a7ec723a09264ada902
roman.lesnikov@gmail.com <roman.lesnikov@gmail.com>
2022-12-15 15:50:33 +0200

    #4145 - Uploader's enhancement (css)

--- plugins_public/tailwind/js/tailwind.config.js
+++ plugins_public/tailwind/js/tailwind.config.js
@@ -79,7 +79,7 @@ module.exports = {
 
     'flickity-button',
 	  
-    'ql-editor', 'space-x-4',
+    'ql-editor', 'space-x-4', 'filepond--drop-label',
       
     'bg-slate-600','bg-gray-600','bg-zinc-600','bg-neutral-600','bg-stone-600','bg-red-600','bg-orange-600','bg-amber-600','bg-yellow-600','bg-lime-600','bg-green-600','bg-emerald-600','bg-teal-600','bg-cyan-600','bg-sky-600','bg-indigo-600','bg-violet-600','bg-blue-600','bg-purple-600','bg-fuchsia-600','bg-pink-600','bg-rose-600',
       
