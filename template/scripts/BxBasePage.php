0715e10139e5e44648ea32fb4e1640029e370257
Alex Trofimov <alex.trofimov@boonex.com>
2022-12-19 15:58:50 +1100

    #4155 Wiki: link styles

--- template/scripts/BxBasePage.php
+++ template/scripts/BxBasePage.php
@@ -763,7 +763,7 @@ class BxBasePage extends BxDolPage
             $sContent = $oWiki->getBlockContent($aBlock['id'], false, (int)bx_get($aBlock['id'].'rev') ? (int)bx_get($aBlock['id'].'rev') : false);
         }
 
-        $s = '<div id="bx-page-wiki-container-' . $aBlock['id'] . '" class="bx-page-wiki-container markdown-body">' . $sContent . '</div>';
+        $s = '<div id="bx-page-wiki-container-' . $aBlock['id'] . '" class="bx-page-wiki-container markdown-body bx-def-vanilla-html">' . $sContent . '</div>';
         $s = $this->_replaceMarkers($s, array('block_id' => $aBlock['id']));
         $s = bx_process_macros($s);
         return $s;
