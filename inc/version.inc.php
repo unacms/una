c57ac7236f44aa330f4c028547fb5f3e6912bf36
Alex Trofimov <alex.trofimov@boonex.com>
2023-02-23 16:32:33 +1100

    v.13.0.0-RC3

--- inc/version.inc.php
+++ inc/version.inc.php
@@ -7,6 +7,6 @@
  * @{
  */
 
-define('BX_DOL_VERSION', '13.0.0-RC2'); ///< system files version, for DB version @see bx_get_ver, DB and files version must match
+define('BX_DOL_VERSION', '13.0.0-RC3'); ///< system files version, for DB version @see bx_get_ver, DB and files version must match
 
 /** @} */
