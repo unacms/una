eadb670c2f05899ba19d0892c4428a56ab09700e
Alex Trofimov <alex.trofimov@boonex.com>
2023-02-28 11:07:02 +1100

    #4256 Artificer template isn't installed by default on UNA Cloud

--- install/classes/BxDolInstallSiteConfig.php
+++ install/classes/BxDolInstallSiteConfig.php
@@ -206,7 +206,7 @@ class BxDolInstallSiteConfig
             BX_DOL_MODULE_TYPE_TEMPLATE => array(
                 'name' => _t('_sys_inst_conf_field_template'),
                 'desc' => _t('_sys_inst_conf_desc_template'),
-                'def' => 'protean',
+                'def' => 'artificer',
                 'func' => 'rowSelect',
                 'vals' => $this->getSelectValues(BX_DOL_MODULE_TYPE_TEMPLATE),
             ),
