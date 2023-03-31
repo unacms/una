c888e86f3a662ec8527ce1cdeffee392ec12d368
AntonLV <vlnotna@gmail.com>
2023-03-31 14:34:15 +0300

    Modules updates.

---
+++ modules/boonex/russian/updates/13.0.8_13.0.9/install/config.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
+ */
+
+$aConfig = array(
+    /**
+     * Main Section.
+     */
+    'title' => 'Russian',
+    'version_from' => '13.0.8',
+    'version_to' => '13.0.9',
+    'vendor' => 'BoonEx',
+
+    'compatible_with' => array(
+        '13.0.0-RC4'
+    ),
+
+    /**
+     * 'home_dir' and 'home_uri' - should be unique. Don't use spaces in 'home_uri' and the other special chars.
+     */
+    'home_dir' => 'boonex/russian/updates/update_13.0.8_13.0.9/',
+    'home_uri' => 'ru_update_1308_1309',
+
+    'module_dir' => 'boonex/russian/',
+    'module_uri' => 'ru',
+
+    'db_prefix' => 'bx_rsn_',
+    'class_prefix' => 'BxRsn',
+
+    /**
+     * Installation/Uninstallation Section.
+     */
+    'install' => array(
+        'execute_sql' => 0,
+        'update_files' => 1,
+        'update_languages' => 1,
+        'restore_languages' => 0,
+        'clear_db_cache' => 0,
+    ),
+
+    /**
+     * Category for language keys.
+     */
+    'language_category' => array(
+        array('name' => 'Accounts', 'path' => 'bx_accounts/'),
+        array('name' => 'Events', 'path' => 'bx_events/'),
+        array('name' => 'System', 'path' => 'system/'),
+    ),
+
+    /**
+     * Files Section
+     */
+    'delete_files' => array(),
+);
