f835a7a1ba1943fbba150be650ce1cc1eddfdbbe
Alex Trofimov <alex.trofimov@boonex.com>
2023-04-24 13:58:57 +0300

    Upgrade script from 13.0.0-RC4 to 13.0.0-RC5

---
+++ upgrade/files/13.0.0.RC4-13.0.0.RC5/files_delete.php
@@ -0,0 +1,14 @@
+<?php
+
+$aFilesDelete = array(
+
+    'plugins_public/emoji',
+
+//	'plugins/phpmailer/Exception.php',
+//	'plugins/phpmailer/LICENSE',
+//	'plugins/phpmailer/PHPMailer.php',
+//	'plugins/phpmailer/README.md',
+//	'plugins/phpmailer/SMTP.php',
+//	'plugins/phpmailer/VERSION',
+);
+
