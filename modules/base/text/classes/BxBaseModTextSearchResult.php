22398ffbbbb74293a9a0790ce829eb699252c80b
AntonLV <vlnotna@gmail.com>
2023-05-12 14:36:27 +0300

    Ticket #4148 - Build-in API support: Remove duplicated Author data in Content snippets.

--- modules/base/text/classes/BxBaseModTextSearchResult.php
+++ modules/base/text/classes/BxBaseModTextSearchResult.php
@@ -241,13 +241,18 @@ class BxBaseModTextSearchResult extends BxBaseModGeneralSearchResult
     {
         $CNF = &$this->oModule->_oConfig->CNF;
 
+        $bMetaMenu = false;
+        /**
+         * Disabled for now, because Author is used only.
         $oMetaMenu = !empty($CNF['OBJECT_MENU_SNIPPET_META']) ? BxDolMenu::getObjectInstance($CNF['OBJECT_MENU_SNIPPET_META'], $this->oModule->_oTemplate) : false;
         $bMetaMenu = $oMetaMenu !== false;
+         */
 
         $oContentInfo = $this->getContentInfoObject();
 
         foreach ($a as $i => $r) {
             $a[$i] = array_merge($a[$i], [
+                'author_data' => isset($r['author']) ? BxDolProfile::getData($r[$CNF['FIELD_AUTHOR']]) : '',
                 'url' => $this->decodeDataUrl($oContentInfo, $r),
                 'image' => $oContentInfo->getContentThumb($r['id']),
                 'summary_plain' => $this->decodeDataSummaryPlain($oContentInfo, $r)
