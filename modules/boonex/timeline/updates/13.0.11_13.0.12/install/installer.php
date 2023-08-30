84c7e35c6cef5dc4cb1f239fca885beba4d803b9
AntonLV <vlnotna@gmail.com>
2023-08-30 07:31:32 +0300

    Modules updates.

--- modules/boonex/timeline/updates/13.0.11_13.0.12/install/installer.php
+++ modules/boonex/timeline/updates/13.0.11_13.0.12/install/installer.php
@@ -10,4 +10,14 @@ class BxTimelineUpdater extends BxDolStudioUpdater
     {
         parent::__construct($aConfig);
     }
+
+    public function actionExecuteSql($sOperation)
+    {
+        if($sOperation == 'install') {
+            if(!$this->oDb->isFieldExists('bx_timeline_events', 'source'))
+                $this->oDb->query("ALTER TABLE `bx_timeline_events` ADD `source` varchar(32) NOT NULL default '' AFTER `content`");
+        }
+
+        return parent::actionExecuteSql($sOperation);
+    }
 }
