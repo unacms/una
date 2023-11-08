741e4aa28800e0e3326e6fb0265a51a4fad82bfc
AntonLV <vlnotna@gmail.com>
2023-11-08 13:13:57 +0300

    Ticket #4543 - Invitations: Allow to invite on a definite page.

--- modules/boonex/invites/install/sql/enable.sql
+++ modules/boonex/invites/install/sql/enable.sql
@@ -32,6 +32,11 @@ SET @iPBOrderDashboard = 2; --(SELECT IFNULL(MAX(`order`), 0) FROM `sys_pages_bl
 INSERT INTO `sys_pages_blocks` (`object`, `cell_id`, `module`, `title`, `designbox_id`, `visible_for_levels`, `type`, `content`, `deletable`, `copyable`, `order`) VALUES
 ('sys_dashboard', @iPBCellDashboard, @sName, '_bx_invites_page_block_title_invite', 11, 2147483647, 'service', 'a:2:{s:6:"module";s:10:"bx_invites";s:6:"method";s:16:"get_block_invite";}', 0, 1, @iPBOrderDashboard);
 
+-- PAGE: service blocks
+SET @iBlockOrder = (SELECT `order` FROM `sys_pages_blocks` WHERE `object` = '' AND `cell_id` = 0 ORDER BY `order` DESC LIMIT 1);
+INSERT INTO `sys_pages_blocks`(`object`, `cell_id`, `module`, `title_system`, `title`, `designbox_id`, `visible_for_levels`, `type`, `content`, `deletable`, `copyable`, `active`, `order`) VALUES 
+('', 0, @sName, '_bx_invites_page_block_title_system_invite_with_redirect', '_bx_invites_page_block_title_invite', 11, 2147483647, 'service', 'a:3:{s:6:"module";s:10:"bx_invites";s:6:"method";s:16:"get_block_invite";s:6:"params";a:1:{i:0;b:1;}}', 0, 1, 1, @iBlockOrder + 1);
+
 
 -- MENU: dashboard manage tools
 SET @iManageMenuOrder = (SELECT IFNULL(MAX(`order`), 0) FROM `sys_menu_items` WHERE `set_name`='sys_account_dashboard_manage_tools' LIMIT 1);
