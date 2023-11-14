b4de1567e2d132efc26a01a2f439e0d940e3f0db
AntonLV <vlnotna@gmail.com>
2023-11-14 16:34:31 +0300

    Ticket #4546 - Recursion call of profile unit.

--- modules/boonex/payment/classes/BxPaymentCart.php
+++ modules/boonex/payment/classes/BxPaymentCart.php
@@ -394,7 +394,6 @@ class BxPaymentCart extends BxBaseModPaymentCart
             'vendor_icon' => $aSeller['icon'],
             'vendor_thumb' => $aSeller['thumb'],
             'vendor_avatar' => $aSeller['avatar'],
-            'vendor_unit' => $aSeller['unit'],
             'vendor_currency_code' => $aSeller['currency_code'],
             'vendor_currency_sign' => $aSeller['currency_sign'],
             'items_count' => $iItemsCount,
