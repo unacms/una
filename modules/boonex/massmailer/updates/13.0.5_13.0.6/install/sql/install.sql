914e48da35746798ae11ef63e8c1a32b8ac4909e
AntonLV <vlnotna@gmail.com>
2023-11-20 11:58:24 +0300

    Modules updates. Mass mailer.

--- modules/boonex/massmailer/updates/13.0.5_13.0.6/install/sql/install.sql
+++ modules/boonex/massmailer/updates/13.0.5_13.0.6/install/sql/install.sql
@@ -2,10 +2,13 @@ SET @sName = 'bx_massmailer';
 
 
 -- FORMS
-DELETE FROM `sys_form_inputs` WHERE `object`=@sName AND `name`='controls';
-UPDATE `sys_form_inputs` SET `name`='controls' WHERE `object`=@sName AND `name`='controls2';
+DELETE FROM `sys_form_inputs` WHERE `object`=@sName AND `name` IN ('controls', 'controls2');
+INSERT INTO `sys_form_inputs` (`object`, `module`, `name`, `value`, `values`, `checked`, `type`, `caption_system`, `caption`, `info`, `required`, `unique`, `collapsed`, `html`, `attrs`, `attrs_tr`, `attrs_wrapper`, `checker_func`, `checker_params`, `checker_error`, `db_pass`, `db_params`, `editable`, `deletable`) VALUES
+(@sName, @sName, 'controls', '', 'do_send,cancel', 0, 'input_set', '', '', '', 0, 0, 0, 0, '', '', '', '', '', '', '', '', 0, 0);
 
 DELETE FROM `sys_form_display_inputs` WHERE `display_name` IN ('bx_massmailer_campaign_add', 'bx_massmailer_campaign_edit') AND `input_name` IN ('controls', 'cancel');
 
-DELETE FROM `sys_form_display_inputs` WHERE `input_name`='controls' AND `display_name` IN ('bx_massmailer_campaign_send_test', 'bx_massmailer_campaign_send_all');
-UPDATE `sys_form_display_inputs` SET `input_name`='controls' WHERE `display_name` IN ('bx_massmailer_campaign_send_test', 'bx_massmailer_campaign_send_all') AND `input_name`='controls2';
+DELETE FROM `sys_form_display_inputs` WHERE `input_name` IN ('controls', 'controls2') AND `display_name` IN ('bx_massmailer_campaign_send_test', 'bx_massmailer_campaign_send_all');
+INSERT INTO `sys_form_display_inputs` (`display_name`, `input_name`, `visible_for_levels`, `active`, `order`) VALUES
+('bx_massmailer_campaign_send_test', 'controls', 2147483647, 1, 2),
+('bx_massmailer_campaign_send_all', 'controls', 2147483647, 1, 2);
