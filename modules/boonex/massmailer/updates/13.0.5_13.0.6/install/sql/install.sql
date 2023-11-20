3f69a5c5f30445b826422cbe1c9a168d6fb7c920
AntonLV <vlnotna@gmail.com>
2023-11-20 10:00:12 +0300

    Modules updates. Mass mailer.

--- modules/boonex/massmailer/updates/13.0.5_13.0.6/install/sql/install.sql
+++ modules/boonex/massmailer/updates/13.0.5_13.0.6/install/sql/install.sql
@@ -6,4 +6,6 @@ DELETE FROM `sys_form_inputs` WHERE `object`=@sName AND `name`='controls';
 UPDATE `sys_form_inputs` SET `name`='controls' WHERE `object`=@sName AND `name`='controls2';
 
 DELETE FROM `sys_form_display_inputs` WHERE `display_name` IN ('bx_massmailer_campaign_add', 'bx_massmailer_campaign_edit') AND `input_name` IN ('controls', 'cancel');
+
+DELETE FROM `sys_form_display_inputs` WHERE `input_name`='controls' AND `display_name` IN ('bx_massmailer_campaign_send_test', 'bx_massmailer_campaign_send_all');
 UPDATE `sys_form_display_inputs` SET `input_name`='controls' WHERE `display_name` IN ('bx_massmailer_campaign_send_test', 'bx_massmailer_campaign_send_all') AND `input_name`='controls2';
