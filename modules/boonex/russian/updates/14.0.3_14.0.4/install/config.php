26801fca4d5f969329ed861347a57364d9f4ddf8
AntonLV <vlnotna@gmail.com>
2024-06-27 09:28:59 +0300

    Modules updates.

---
+++ modules/boonex/russian/updates/14.0.3_14.0.4/install/config.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
+ */
+
+$aConfig = array(
+    /**
+     * Main Section.
+     */
+    'title' => 'Russian',
+    'version_from' => '14.0.3',
+    'version_to' => '14.0.4',
+    'vendor' => 'UNA INC',
+
+    'compatible_with' => array(
+        '14.0.0-B2'
+    ),
+
+    /**
+     * 'home_dir' and 'home_uri' - should be unique. Don't use spaces in 'home_uri' and the other special chars.
+     */
+    'home_dir' => 'boonex/russian/updates/update_14.0.3_14.0.4/',
+    'home_uri' => 'ru_update_1403_1404',
+
+    'module_dir' => 'boonex/russian/',
+    'module_uri' => 'ru',
+
+    'db_prefix' => 'bx_rsn_',
+    'class_prefix' => 'BxRsn',
+
+    /**
+     * Installation/Uninstallation Section.
+     */
+    'install' => array(
+        'execute_sql' => 0,
+        'update_files' => 1,
+        'update_languages' => 1,
+        'restore_languages' => 0,
+        'clear_db_cache' => 0,
+    ),
+
+    /**
+     * Category for language keys.
+     */
+    'language_category' => array(
+        array('name' => 'System', 'path' => 'system/'),
+    ),
+
+    /**
+     * Files Section
+     */
+    'delete_files' => array(),
+);
