b61bcf603bdb7715c5029e7326b6bb84b4500110
Alex Trofimov <alex.trofimov@boonex.com>
2024-09-12 15:03:37 +0600

    #4800 Add replaceable markers for pages blocks for currently logged profile

--- template/scripts/BxBasePage.php
+++ template/scripts/BxBasePage.php
@@ -52,11 +52,11 @@ class BxBasePage extends BxDolPage
         $this->addMarkers(array('site_title' => getParam('site_title')));
         if (isLogged() && $oProfile = BxDolProfile::getInstance()) {
             $this->addMarkers([
-                'logged_profile_id' => $oProfile->id(),;
-                'logged_account_id' => $oProfile->getAccountId()]);
-                'logged_profile_module' => $oProfile->getModule()]);
-                'logged_display_name' => $oProfile->getDisplayName()]);
-                'logged_profile_url' => $oProfile->getUrl()
+                'logged_profile_id' => $oProfile->id(),
+                'logged_account_id' => $oProfile->getAccountId(),
+                'logged_profile_module' => $oProfile->getModule(),
+                'logged_display_name' => $oProfile->getDisplayName(),
+                'logged_profile_url' => $oProfile->getUrl(),
             ]);
         }
         if (isset($GLOBALS['bxReplaceableMarkersPages'])) {
