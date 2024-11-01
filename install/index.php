cb03c6a5cb2243eb403c711281fffdde31f8b0e3
Alex Trofimov <alex.trofimov@boonex.com>
2024-11-01 21:51:03 +0300

    Back to dev version

--- install/index.php
+++ install/index.php
@@ -10,7 +10,7 @@
 define ('BX_DOL_INSTALL',  true);
 define ('BX_SKIP_INSTALL_CHECK', true);
 
-define ('BX_DOL_VER', '14.0.0-RC2');
+define ('BX_DOL_VER', '14.0.0-DEV8');
 
 define ('BX_INSTALL_DEFAULT_LANGUAGE', 'en');
 
