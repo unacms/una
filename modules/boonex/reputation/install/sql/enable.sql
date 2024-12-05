358b66f5c5e708458af6db9b3f50d56a42756606
AntonLV <vlnotna@gmail.com>
2024-12-05 15:42:09 +0300

    Ticket #4844 - Reputation module.

--- modules/boonex/reputation/install/sql/enable.sql
+++ modules/boonex/reputation/install/sql/enable.sql
@@ -4,7 +4,7 @@ SET @sName = 'bx_reputation';
 -- SETTINGS
 SET @iTypeOrder = (SELECT MAX(`order`) FROM `sys_options_types` WHERE `group` = 'modules');
 INSERT INTO `sys_options_types`(`group`, `name`, `caption`, `icon`, `order`) VALUES 
-('modules', @sName, '_bx_reputation', 'bx_reputation@modules/boonex/ewts/|std-icon.svg', IF(ISNULL(@iTypeOrder), 1, @iTypeOrder + 1));
+('modules', @sName, '_bx_reputation', 'bx_reputation@modules/boonex/reputation/|std-icon.svg', IF(ISNULL(@iTypeOrder), 1, @iTypeOrder + 1));
 SET @iTypeId = LAST_INSERT_ID();
 
 INSERT INTO `sys_options_categories` (`type_id`, `name`, `caption`, `order`)
