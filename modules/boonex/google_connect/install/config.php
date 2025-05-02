ec7717407793a90278ec4dad6d5364e6e98ca4e9
AntonLV <vlnotna@gmail.com>
2025-05-02 15:07:10 +0300

    Ticket #4577 - Change vendor for all modules. Google Connect.

--- modules/boonex/google_connect/install/config.php
+++ modules/boonex/google_connect/install/config.php
@@ -18,7 +18,7 @@ $aConfig = array(
     'title' => 'Google connect',
     'note' => 'Join the site using LinkedIn account.',
     'version' => '13.0.2.DEV',
-    'vendor' => 'Boonex',
+    'vendor' => 'UNA INC',
     'help_url' => 'http://feed.una.io/?section={module_name}',
 
     'compatible_with' => array(
